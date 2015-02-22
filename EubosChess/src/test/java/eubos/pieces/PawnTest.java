0a51270f4907d9860478f80706cc5f00252236bc
Chris Bolt <c_j_bolt@yahoo.co.uk>
2015-02-22 13:53:19 +0000

    Added first Junit tests for pawn movement

---
+++ EubosChess/src/test/java/eubos/pieces/PawnTest.java
@@ -0,0 +1,63 @@
+package eubos.pieces;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public abstract class PawnTest {
+
+	@Test
+	public void testInitial_MoveOneSquare() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInitial_MoveTwoSquares() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInitial_EnPassant() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_MoveOneSquare() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_CaptureLeft() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_CaptureRight() {
+		fail("Not yet implemented");
+	}
+		
+	@Test
+	public void test_PromoteQueen() {
+		fail("Not yet implemented");
+	}	
+	
+	@Test
+	public void test_PromoteKnight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test_PromoteBishop() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_PromoteRook() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPawn() {
+		fail("Not yet implemented");
+	}
+}
