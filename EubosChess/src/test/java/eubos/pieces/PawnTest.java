774132463195f27a7ada397fa4ac92a1a79bdda2
Chris Bolt <c_j_bolt@yahoo.co.uk>
2015-02-23 12:14:47 +0000

    Update abstract TestPawn class.

--- EubosChess/src/test/java/eubos/pieces/PawnTest.java
+++ EubosChess/src/test/java/eubos/pieces/PawnTest.java
@@ -1,58 +1,36 @@
 package eubos.pieces;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public abstract class PawnTest {
 
 	@Test
-	public void testInitial_MoveOneSquare() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_InitialMoveOneSquare();
 	
 	@Test
-	public void testInitial_MoveTwoSquares() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_InitialMoveTwoSquares();
 	
 	@Test
-	public void testInitial_EnPassant() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_CaptureEnPassant();
 	
 	@Test
-	public void test_MoveOneSquare() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_MoveOneSquare();
 	
 	@Test
-	public void test_CaptureLeft() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_CaptureLeft();
 	
 	@Test
-	public void test_CaptureRight() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_CaptureRight();
 		
 	@Test
-	public void test_PromoteQueen() {
-		fail("Not yet implemented");
-	}	
+	public abstract void test_PromoteQueen();
 	
 	@Test
-	public void test_PromoteKnight() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_PromoteKnight();
 
 	@Test
-	public void test_PromoteBishop() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_PromoteBishop();
 	
 	@Test
-	public void test_PromoteRook() {
-		fail("Not yet implemented");
-	}
+	public abstract void test_PromoteRook();
 }
