b70ab327c483b8bb3523cbfe859b8b1dce4c0900
Chris Bolt <c_j_bolt@yahoo.co.uk>
2015-03-15 21:50:14 +0000

    Completed implementation of all legal chess moves.
    Have not implemented stale mate, 50 move role or three fold repeated
    position. this is still todo. Now need to replace random move generation
    with a basic minimax tree search...

--- EubosChess/src/main/java/eubos/pieces/Knight.java
+++ EubosChess/src/main/java/eubos/pieces/Knight.java
@@ -1,9 +1,11 @@
 package eubos.pieces;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import com.fluxchess.jcpi.models.*;
 
+import eubos.board.Board;
 import eubos.board.BoardManager;
 
 public class Knight extends IndirectMovePiece {
@@ -12,17 +14,107 @@ public class Knight extends IndirectMovePiece {
 		colour = Colour;
 		onSquare = at;
 	}
+
+	private GenericPosition getUpRight() {
+		if ( onSquare.file != GenericFile.Fh && ((onSquare.rank != GenericRank.R8) && (onSquare.rank != GenericRank.R7))) {
+			return GenericPosition.valueOf( onSquare.file.next(), onSquare.rank.next().next());	
+		}
+		return null;
+	}
+	
+	private GenericPosition getUpLeft() {
+		if ( onSquare.file != GenericFile.Fa && ((onSquare.rank != GenericRank.R8) && (onSquare.rank != GenericRank.R7))) {
+			return GenericPosition.valueOf( onSquare.file.prev(), onSquare.rank.next().next());
+		}
+		return null;
+	}
+	
+	private GenericPosition getDownRight() {
+		if ( onSquare.file != GenericFile.Fh && ((onSquare.rank != GenericRank.R1) && (onSquare.rank != GenericRank.R2))) {
+			return GenericPosition.valueOf( onSquare.file.next(), onSquare.rank.prev().prev());
+		}
+		return null;
+	}
+	
+	private GenericPosition getDownLeft() {
+		if ( onSquare.file != GenericFile.Fa && ((onSquare.rank != GenericRank.R1) && (onSquare.rank != GenericRank.R2))) {
+			return GenericPosition.valueOf( onSquare.file.prev(), onSquare.rank.prev().prev());
+		}
+		return null;
+	}
+
+	private GenericPosition getRightUp() {
+		if (((onSquare.file != GenericFile.Fg) && (onSquare.file != GenericFile.Fh)) && onSquare.rank != GenericRank.R8 ) {
+			return GenericPosition.valueOf( onSquare.file.next().next(), onSquare.rank.next());
+		}
+		return null;
+	}	
+	
+	private GenericPosition getRightDown() {
+		if (((onSquare.file != GenericFile.Fg) && (onSquare.file != GenericFile.Fh)) && onSquare.rank != GenericRank.R1  ) {
+			return GenericPosition.valueOf( onSquare.file.next().next(), onSquare.rank.prev());
+		}
+		return null;
+	}	
+
+	private GenericPosition getLeftUp() {
+		if (((onSquare.file != GenericFile.Fa) && (onSquare.file != GenericFile.Fb)) && onSquare.rank != GenericRank.R8 )  {
+			return GenericPosition.valueOf( onSquare.file.prev().prev(), onSquare.rank.next());
+		}
+		return null;
+	}	
+	
+	private GenericPosition getLeftDown() {
+		if (((onSquare.file != GenericFile.Fa) && (onSquare.file != GenericFile.Fb)) && onSquare.rank != GenericRank.R1 )  {
+			return GenericPosition.valueOf( onSquare.file.prev().prev(), onSquare.rank.prev());
+		}
+		return null;
+	}
+	
+	private void checkAddMove(LinkedList<GenericMove> moveList, Board theBoard, GenericPosition targetSquare) {
+		if ( targetSquare != null ) {
+			Piece targetPiece = theBoard.getPieceAtSquare(targetSquare);
+			if (theBoard.squareIsEmpty(targetSquare)) {
+				moveList.add( new GenericMove( onSquare, targetSquare ));
+			}
+			else if (targetPiece != null && isOppositeColour(targetPiece)) {
+				// Indicates a capture
+				moveList.add( new GenericMove( onSquare, targetSquare ));
+			}
+			// Indicates blocked by own piece.
+		}
+	}
 	
 	@Override
 	public LinkedList<GenericMove> generateMoves(BoardManager bm) {
 		LinkedList<GenericMove> moveList = new LinkedList<GenericMove>();
+		Board theBoard = bm.getTheBoard();
+		checkAddMove(moveList, theBoard, getUpRight());
+		checkAddMove(moveList, theBoard, getUpLeft());
+		checkAddMove(moveList, theBoard, getRightUp());
+		checkAddMove(moveList, theBoard, getRightDown());
+		checkAddMove(moveList, theBoard, getDownRight());
+		checkAddMove(moveList, theBoard, getDownLeft());
+		checkAddMove(moveList, theBoard, getLeftUp());
+		checkAddMove(moveList, theBoard, getLeftDown());
 		return moveList;		
 	}
 
 	@Override
 	public boolean attacks(GenericPosition pos) {
-		// TODO Auto-generated method stub
-		return false;
+		ArrayList<GenericPosition> targetSqs = new ArrayList<GenericPosition>();
+		targetSqs.add(getUpRight());
+		targetSqs.add(getUpLeft());
+		targetSqs.add(getRightUp());
+		targetSqs.add(getRightDown());
+		targetSqs.add(getDownRight());
+		targetSqs.add(getDownLeft());
+		targetSqs.add(getLeftUp());
+		targetSqs.add(getLeftDown());
+		if (targetSqs.contains(pos))
+			return true;
+		else
+			return false;
 	}
 
 }
---
+++ EubosChess/src/test/java/eubos/pieces/KnightTest.java
@@ -0,0 +1,56 @@
+package eubos.pieces;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import com.fluxchess.jcpi.models.GenericMove;
+import com.fluxchess.jcpi.models.GenericPosition;
+
+import eubos.pieces.Piece.Colour;
+
+public class KnightTest extends PieceTest {
+	
+	GenericPosition startTestOnSq;
+	
+	@Test
+	public void test_SquareA8() {
+		startTestOnSq = GenericPosition.a8;
+		classUnderTest = new Knight( Colour.white, startTestOnSq );
+		LinkedList<GenericMove> ml = completeSetupAndGenerateMoves();
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.c7 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.b6 ));
+		expectedNumMoves = 2;
+		checkExpectedMoves(ml);
+	}
+	
+	@Test
+	public void test_SquareB7() {
+		startTestOnSq = GenericPosition.b7;
+		classUnderTest = new Knight( Colour.white, startTestOnSq);
+		LinkedList<GenericMove> ml = completeSetupAndGenerateMoves();
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.d8 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.d6 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.a5 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.c5 ));
+		expectedNumMoves = 4;
+		checkExpectedMoves(ml);
+	}
+	
+	@Test
+	public void test_SquareC6() {
+		startTestOnSq = GenericPosition.c6;
+		classUnderTest = new Knight( Colour.white, startTestOnSq);
+		LinkedList<GenericMove> ml = completeSetupAndGenerateMoves();
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.b8 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.d8 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.e7 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.e5 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.b4 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.d4 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.a7 ));
+		expectedMoves.add( new GenericMove( startTestOnSq, GenericPosition.a5 ));
+		expectedNumMoves = 8;
+		checkExpectedMoves(ml);
+	}	
+}
