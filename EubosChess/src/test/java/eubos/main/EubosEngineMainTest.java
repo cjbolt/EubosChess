c808bc349377fd8f62800038ce3058055f2b20a5
cb20 <cb20@CB20LAP03.EUROPE.ROOT.PRI>
2015-04-15 17:58:15 +0100

    Updated with sniffer for System.out

--- EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
+++ EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
@@ -3,7 +3,9 @@ package eubos.main;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
 import org.junit.After;
@@ -15,6 +17,8 @@ public class EubosEngineMainTest {
 	private EubosEngineMain eubos;
 	private Thread eubosThread;
 	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
 	@Before
 	public void setUp() {
 //		// start the Engine
@@ -22,6 +26,11 @@ public class EubosEngineMainTest {
 //		eubosThread = new Thread( eubos );
 //		eubosThread.start();
 	}
+	
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}	
 	
 	@Test
 	public void test_startEngine() throws UnsupportedEncodingException {
@@ -34,7 +43,6 @@ public class EubosEngineMainTest {
 			eubos = new EubosEngineMain();
 			eubosThread = new Thread( eubos );
 			eubosThread.start();
-			System.out.println("started Eubos");
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
@@ -44,6 +52,12 @@ public class EubosEngineMainTest {
 		} finally {
 		    System.setIn( old );
 		}
+		assertEquals("id name Eubos\nid author Chris Bolt\nuciok\n\n", outContent.toString());
+	}
+	
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
 	}
 	
 	@After
