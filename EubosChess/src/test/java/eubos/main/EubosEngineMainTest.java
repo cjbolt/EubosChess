ab3678bc28740dc377ebaea654eac08b949d1213
Chris Bolt <c_j_bolt@yahoo.co.uk>
2015-04-17 14:19:02 +0100

    Refactored the EubosEngineMain unit tests extensively.

--- EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
+++ EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
@@ -2,13 +2,10 @@ package eubos.main;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PipedWriter;
 import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -19,107 +16,45 @@ import org.junit.Test;
 public class EubosEngineMainTest {
 
 	private EubosEngineMain classUnderTest;
-	
 	private Thread eubosThread;
-	private final ByteArrayOutputStream testOutput = new ByteArrayOutputStream();
-	private InputStream testInput;
 	
+	// Command Lists emptied in main test loop.
+	private ArrayList<String> inputCmds = new ArrayList<String>();
+	private ArrayList<String> outputCmds = new ArrayList<String>();
+	
+	// Test infrastructure to allow pushing commands into Eubos and sniffing them out.
+	private PipedWriter inputToEngine;
+	private final ByteArrayOutputStream testOutput = new ByteArrayOutputStream();
+	
+	// Command building blocks
 	private static final String CMD_TERMINATOR = "\r\n";
-	// Engine inputs
+	private static final String POS_FEN_PREFIX = "position fen ";
+//	private static final String POS_START_PREFIX = "position startpos ";
+	private static final String BEST_PREFIX = "bestmove ";
+	
+	// Whole Commands
+	// Inputs
 	private static final String UCI_CMD = "uci"+CMD_TERMINATOR;
 	private static final String ISREADY_CMD = "isready"+CMD_TERMINATOR;
 	private static final String NEWGAME_CMD = "ucinewgame"+CMD_TERMINATOR;
-	private static final String POS_FEN_PREFIX = "position fen ";
-//	private static final String POS_START_PREFIX = "position startpos ";
 	private static final String GO_CMD = "go infinite"+CMD_TERMINATOR;
 //	private static final String QUIT_CMD = "quit"+CMD_TERMINATOR;
-	// Engine outputs
+	// Outputs
 	private static final String ID_NAME_CMD = "id name Eubos"+CMD_TERMINATOR;
 	private static final String ID_AUTHOR_CMD = "id author Chris Bolt"+CMD_TERMINATOR;
 	private static final String UCI_OK_CMD = "uciok"+CMD_TERMINATOR;
 	private static final String READY_OK_CMD = "readyok"+CMD_TERMINATOR;
-	private static final String BEST_PREFIX = "bestmove ";
+	
+	private static final int sleep_10ms = 10;
 	
 	@Before
-	public void setUp() {
-	}
-	
-	@Before
-	public void setUpStreams() {
-	    System.setOut(new PrintStream(testOutput));
-	}	
-	
-	@Test
-	public void test_startEngine() throws UnsupportedEncodingException, InterruptedException {
-		testInput = new ByteArrayInputStream( UCI_CMD.getBytes("UTF-8") );
-		InputStream old = System.in;
-		try {
-		    System.setIn( testInput );
-			// Start the Engine
-			classUnderTest = new EubosEngineMain();
-			eubosThread = new Thread( classUnderTest );
-			eubosThread.start();
-			// Give the engine thread some cpu time
-			Thread.sleep(50);
-		} finally {
-		    System.setIn( old );
-		}
-		assertEquals(ID_NAME_CMD+ID_AUTHOR_CMD+UCI_OK_CMD,testOutput.toString());
-	}
-	
-	@Test
-	public void test_mateInOne() throws InterruptedException, IOException {
-		// Setup input commands
-		ArrayList<String> inputCmds = new ArrayList<String>();
-		inputCmds.add(UCI_CMD);
-		inputCmds.add(ISREADY_CMD);
-		inputCmds.add(NEWGAME_CMD);
-		inputCmds.add(ISREADY_CMD);
-		inputCmds.add(POS_FEN_PREFIX+"k1K5/b7/R7/1P6/1n6/8/8/8 w - - 0 1"+CMD_TERMINATOR);
-		inputCmds.add(GO_CMD);
-		// Setup output commands
-		ArrayList<String> outputCmds = new ArrayList<String>();
-		outputCmds.add(ID_NAME_CMD+ID_AUTHOR_CMD+UCI_OK_CMD);
-		outputCmds.add(READY_OK_CMD);
-		outputCmds.add(null);
-		outputCmds.add(READY_OK_CMD);
-		outputCmds.add(null);
-		outputCmds.add(BEST_PREFIX+"b5b6"+CMD_TERMINATOR);		
+	public void setUp() throws IOException {
 		// Start engine
-		PipedWriter inputToEngine = new PipedWriter();
+		System.setOut(new PrintStream(testOutput));
+		inputToEngine = new PipedWriter();
 		classUnderTest = new EubosEngineMain(inputToEngine);
 		eubosThread = new Thread( classUnderTest );
 		eubosThread.start();
-		// Apply inputs and check outputs...
-		for (String currCmd: inputCmds) {
-			// Pass command to engine
-			inputToEngine.write(currCmd);
-			// Give the engine thread some cpu time
-			Thread.sleep(1500);
-			// Get the command to check for...
-			String expectedOutput = outputCmds.remove(0);
-			if (expectedOutput != null) {
-				// ignore any line starting with info
-				String recievedCmd = testOutput.toString();
-				String parsedCmd = "";
-				String currLine = "";
-				Scanner scan = new Scanner(recievedCmd);
-				while (scan.hasNextLine()) {
-					currLine = scan.nextLine();
-					if (!currLine.contains("info")) {
-						parsedCmd += (currLine + CMD_TERMINATOR);
-					}
-				}
-				scan.close();
-				assertEquals(expectedOutput,parsedCmd);
-				testOutput.reset();
-			}
-		}
-	}
-	
-	@After
-	public void cleanUpStreams() {
-	    //System.setOut(null);
 	}
 	
 	@After
@@ -128,4 +63,79 @@ public class EubosEngineMainTest {
 		classUnderTest = null;
 		eubosThread = null;
 	}
+	
+	@Test
+	public void test_startEngine() throws InterruptedException, IOException {
+		// Setup input/output commands
+		setupEngine();
+		performTest(1000);
+	}
+	
+	@Test
+	public void test_mateInOne() throws InterruptedException, IOException {
+		setupEngine();
+		// Setup Commands specific to this test
+		inputCmds.add(POS_FEN_PREFIX+"k1K5/b7/R7/1P6/1n6/8/8/8 w - - 0 1"+CMD_TERMINATOR);
+		inputCmds.add(GO_CMD);
+		outputCmds.add(null);
+		outputCmds.add(BEST_PREFIX+"b5b6"+CMD_TERMINATOR);		
+		performTest(2000);
+	}
+
+	private void performTest(int timeout) throws IOException, InterruptedException {
+		// Apply inputs and check outputs...
+		for (String currCmd: inputCmds) {
+			// Pass command to engine
+			inputToEngine.write(currCmd);
+			inputToEngine.flush();
+			// Get the command to check for...
+			String expectedOutput = outputCmds.remove(0);
+			if (expectedOutput != null) {
+				boolean received = false;
+				int timer = 0;
+				// Recieve message or wait for timeout to expire.
+				while (!received && timer<timeout) {
+					// Give the engine thread some cpu time
+					Thread.sleep(sleep_10ms);
+					timer += sleep_10ms;
+					// ignore any line starting with info
+					String recievedCmd = testOutput.toString();
+					String parsedCmd = filterInfosOut(recievedCmd);
+					if (expectedOutput.equals(parsedCmd))
+						received = true;
+					testOutput.reset();
+				}
+				if (!received) {
+					fail();
+				}
+			}
+		}
+	}
+
+	private void setupEngine() {
+		// Input
+		inputCmds.add(UCI_CMD);
+		inputCmds.add(ISREADY_CMD);
+		inputCmds.add(NEWGAME_CMD);
+		inputCmds.add(ISREADY_CMD);
+		// Output
+		outputCmds.add(ID_NAME_CMD+ID_AUTHOR_CMD+UCI_OK_CMD);
+		outputCmds.add(READY_OK_CMD);
+		outputCmds.add(null);
+		outputCmds.add(READY_OK_CMD);
+	}
+	
+	private String filterInfosOut(String recievedCmd) {
+		String parsedCmd = "";
+		String currLine = "";
+		Scanner scan = new Scanner(recievedCmd);
+		while (scan.hasNextLine()) {
+			currLine = scan.nextLine();
+			if (!currLine.contains("info")) {
+				parsedCmd += (currLine + CMD_TERMINATOR);
+			}
+		}
+		scan.close();
+		return parsedCmd;
+	}	
 }
