e3b60986f2f21a19540841b0199b469ba310e83b
Chris Bolt <c_j_bolt@yahoo.co.uk>
2015-05-31 22:21:58 +0100

    Simplified the findlssb() implementation in the bitBoard iterator. Not sure this version is any quaicker, but it is a lot simpler.

--- EubosChess/src/main/java/eubos/board/BitBoard.java
+++ EubosChess/src/main/java/eubos/board/BitBoard.java
@@ -42,42 +42,14 @@ public class BitBoard implements Iterable<Integer> {
 	public class SetBitsIterator implements Iterator<Integer> {
 
 		private LinkedList<Integer> setBitsIndexes = null;
-		
-		private final int Mod37BitPosition[] = {
-		  32, 0, 1, 26, 2, 23, 27, 0, 3, 16, 24, 30, 28, 11, 0, 13, 4,
-		  7, 17, 0, 25, 22, 31, 15, 29, 10, 12, 6, 0, 21, 14, 9, 5,
-		  20, 8, 19, 18
-		};
-		
+	
 		public SetBitsIterator() {
 			setBitsIndexes = new LinkedList<Integer>();
-			// First do ls 32bits
-			int tempBB = (int) (bitBoard & 0xffffffff);
-			// handle sign bit being set.
-			if (tempBB < 0) {
-				setBitsIndexes.add(31);
-				tempBB = (int) (bitBoard & 0x7fffffff);
-			}
-			while( tempBB != 0x0L ) {
-				int lowest_set_bit = Integer.lowestOneBit(tempBB);
-				int mod_value = lowest_set_bit % 37;
-				setBitsIndexes.add(Mod37BitPosition[mod_value]);
-				// clears the lsb
-				tempBB &= tempBB-1;
-			}
-			// Then do the ms 32bits
-			tempBB = (int) ((bitBoard >> 32) & 0xffffffff);
-			// handle sign bit being set.
-			if (tempBB < 0) {
-				setBitsIndexes.add(31+32);
-				tempBB = (int) ((bitBoard >> 32) & 0x7fffffff);
-			}
-			while( tempBB != 0x0L ) {
-				int lowest_set_bit = Integer.lowestOneBit(tempBB);
-				int mod_value = lowest_set_bit % 37;
-				setBitsIndexes.add(Mod37BitPosition[mod_value]+32);
-				// clears the lsb
-				tempBB &= tempBB-1;
+			long scratchBitBoard = bitBoard;
+			while ( scratchBitBoard != 0x0L ) {
+				setBitsIndexes.add(Long.numberOfTrailingZeros(scratchBitBoard));
+				// clear the lssb
+				scratchBitBoard &= scratchBitBoard-1;
 			}
 		}
 
