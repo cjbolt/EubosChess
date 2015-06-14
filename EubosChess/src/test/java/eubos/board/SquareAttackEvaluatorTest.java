511d46e8643e3f3d32c20c21b420b47893c80bb3
Chris Bolt <c_j_bolt@yahoo.co.uk>
2015-06-14 14:09:23 +0100

    Updated square attack evaluator unit tests.

--- EubosChess/src/test/java/eubos/board/SquareAttackEvaluatorTest.java
+++ EubosChess/src/test/java/eubos/board/SquareAttackEvaluatorTest.java
@@ -9,19 +9,26 @@ import java.util.List;
 
 
 
+
+
 //import org.junit.Before;
 import org.junit.Test;
 
 import com.fluxchess.jcpi.models.GenericPosition;
 
 import eubos.pieces.Bishop;
+import eubos.pieces.Knight;
 import eubos.pieces.Pawn;
 import eubos.pieces.Piece;
 import eubos.pieces.Piece.Colour;
+import eubos.pieces.Queen;
 
 public class SquareAttackEvaluatorTest {
 
 	private SquareAttackEvaluator classUnderTest = null;
+	// In all the following unit tests, d5 is used as the test square. i.e. each
+	// test revolves around evaluating whether or not this particular square is 
+	// attacked.
 	private GenericPosition testSq = GenericPosition.d5;
 	private List<Piece> pieceList = null;
 	
@@ -57,6 +64,7 @@ public class SquareAttackEvaluatorTest {
 		assertTrue(classUnderTest.isAttacked());
 	}
 
+	// Pawn
 	@Test
 	public void testIsAttacked_AttackedWhitePawnC4() {
 		assertTestSqIsAttacked(new Pawn(Colour.white,GenericPosition.c4)); }
@@ -84,6 +92,8 @@ public class SquareAttackEvaluatorTest {
 	@Test
 	public void testIsAttacked_NotAttackedOwnColourPawnE6() {
 		assertTestSqIsNotAttacked("8/8/4p3/3k4/8/8/8/8 b - - 0 1"); }
+	
+	// Bishop
 	@Test
 	public void testIsAttacked_NotAttackedOwnColourPawnE4() {
 		assertTestSqIsNotAttacked("8/8/8/3K4/4P3/8/8/8 w - - 0 1"); }
@@ -96,4 +106,75 @@ public class SquareAttackEvaluatorTest {
 	@Test
 	public void testIsAttacked_NotAttackedWhiteBishopH1Blocked_fen() {
 		assertTestSqIsNotAttacked("8/8/8/3k4/8/8/6P1/7B b - - 0 1"); }
+	
+	// Knight
+	@Test
+	public void testIsAttacked_AttackedWhiteKnightB4() {
+		assertTestSqIsAttacked(new Knight(Colour.white,GenericPosition.b4)); }
+	@Test
+	public void testIsAttacked_AttackedBlackKnightC3() {
+		assertTestSqIsAttacked(new Knight(Colour.black,GenericPosition.c3)); }
+	@Test
+	public void testIsAttacked_AttackedKnightF6() {
+		assertTestSqIsAttacked(new Knight(Colour.black,GenericPosition.f6)); }
+	@Test
+	public void testIsAttacked_NotAttackedKnightC4() {
+		assertTestSqIsNotAttacked(new Knight(Colour.white,GenericPosition.c4)); }
+	@Test
+	public void testIsAttacked_NotAttackedKnightB3() {
+		assertTestSqIsNotAttacked(new Knight(Colour.white,GenericPosition.b3)); }
+	@Test
+	public void testIsAttacked_NotAttackedKnightC2() {
+		assertTestSqIsNotAttacked(new Knight(Colour.black,GenericPosition.c2)); }
+	@Test
+	public void testIsAttacked_NotAttackedKnightD2() {
+		assertTestSqIsNotAttacked(new Knight(Colour.black,GenericPosition.d2)); }
+	
+	// Queen
+	@Test
+	public void testIsAttacked_AttackedQueenG8() {
+		assertTestSqIsAttacked(new Queen(Colour.white,GenericPosition.g8)); }
+	@Test
+	public void testIsAttacked_AttackedQueenB4() {
+		assertTestSqIsAttacked(new Queen(Colour.white,GenericPosition.d8)); }
+	@Test
+	public void testIsAttacked_AttackedQueenA5() {
+		assertTestSqIsAttacked(new Queen(Colour.black,GenericPosition.a5)); }
+	@Test
+	public void testIsAttacked_AttackedQueenH1() {
+		assertTestSqIsAttacked(new Queen(Colour.black,GenericPosition.h1)); }
+	@Test
+	public void testIsAttacked_NotAttackedQueenC3() {
+		assertTestSqIsNotAttacked(new Queen(Colour.white,GenericPosition.c3)); }
+	@Test
+	public void testIsAttacked_NotAttackedQueenB4() {
+		assertTestSqIsNotAttacked(new Queen(Colour.white,GenericPosition.b4)); }
+	@Test
+	public void testIsAttacked_NotAttackedQueenE3() {
+		assertTestSqIsNotAttacked(new Queen(Colour.black,GenericPosition.e3)); }
+	@Test
+	public void testIsAttacked_NotAttackedQueenE7() {
+		assertTestSqIsNotAttacked(new Queen(Colour.black,GenericPosition.e7)); }
+
+	// Blocked attacks
+	@Test
+	public void testIsAttacked_NotAttacked_BlockedQueenG8() {
+		assertTestSqIsNotAttacked("6Q1/8/4p3/3k4/8/8/8/8 b - - 0 1"); }
+	@Test
+	public void testIsAttacked_NotAttacked_BlockedQueenD8() {
+		assertTestSqIsNotAttacked("3Q4/3p4/8/3k4/8/8/8/8 b - - 0 1"); }
+	@Test
+	public void testIsAttacked_NotAttacked_BlockedQueenA5() {
+		assertTestSqIsNotAttacked("8/3p4/8/Qppk4/8/8/8/8 b - - 0 1"); }
+	
+	// Compound Tests
+	@Test
+	public void testIsAttacked_NotAttacked_Complex1() {
+		assertTestSqIsNotAttacked("B1N5/3p1N2/2P3R1/Qp1k4/1PpP4/8/6q1/Q2R3B b - - 0 1"); }
+	@Test
+	public void testIsAttacked_Attacked_Complex1_QueenA5() {
+		assertTestSqIsAttacked("B1N5/3p1N2/2P3R1/Q2k4/1PpP4/8/6q1/Q2R3B b - - 0 1"); }
+	@Test
+	public void testIsAttacked_Attacked_Illegal1_KingC5() {
+		assertTestSqIsAttacked("B1N5/3p1N2/2P3R1/Q1Kk4/1PpP4/8/6q1/Q2R3B b - - 0 1"); }
 }
