38e6c973be19413a26fd33a0226bbed9932230ad
Chris Bolt <c_j_bolt@yahoo.co.uk>
2019-07-17 20:01:50 +0100

    Start work on enpassant, first capture.

--- EubosChess/src/test/java/eubos/position/ZobristHashCodeTest.java
+++ EubosChess/src/test/java/eubos/position/ZobristHashCodeTest.java
@@ -62,4 +62,30 @@ public class ZobristHashCodeTest {
 
 		assertEquals(initialHashCode, sut.hashCode);	
 	}
+	
+	@Test
+	public void test_update_PerformEnPassantCapture_GivesDiffHashCode() throws Exception {
+		GenericMove move = new GenericMove("f4e3");
+		PositionManager pm = new PositionManager("8/8/8/8/4Pp2/8/8/8 b - e3 0 1");
+		sut = new ZobristHashCode(pm);
+		PositionManager pm_after_capture = new PositionManager("8/8/8/8/8/4p3/8/8 w - - 0 2");
+		ZobristHashCode afterCaptureHashCode = new ZobristHashCode(pm_after_capture);
+		
+		pm.performMove(sut, move);
+
+		assertNotEquals(afterCaptureHashCode.hashCode, sut.hashCode);	
+	}	
+	
+	@Test
+	public void test_update_PerformEnPassantCaptureUnperform_GivesSameHashCode() throws Exception {
+		GenericMove move = new GenericMove("f4e3");
+		PositionManager pm = new PositionManager("8/8/8/8/5p2/8/4P3/8 b - e3 0 1");
+		sut = new ZobristHashCode(pm);
+		long initialHashCode = sut.hashCode;
+		
+		pm.performMove(sut, move);
+		pm.unperformMove(sut);
+
+		assertEquals(initialHashCode, sut.hashCode);	
+	}	
 }
