7e2571146e7bf5141aa3e4a8fc365446f72fd654
Chris Bolt <c_j_bolt@yahoo.co.uk>
2019-09-24 10:45:00 +0100

    Use linked lists and array lists for aging queue in Transposition table

--- EubosChess/src/main/java/eubos/search/FixedSizeTranspositionTable.java
+++ EubosChess/src/main/java/eubos/search/FixedSizeTranspositionTable.java
@@ -1,11 +1,15 @@
 package eubos.search;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class FixedSizeTranspositionTable {
 	
 	private HashMap<Long, Transposition> hashMap = null;
 	private long hashMapSize = 0;
+	private List<Long> ageList = null;
 	
 	public long getHashMapSize() {
 		return hashMapSize;
@@ -15,6 +19,8 @@ public class FixedSizeTranspositionTable {
 	
 	public FixedSizeTranspositionTable() {
 		hashMap = new HashMap<Long, Transposition>((int)MAX_SIZE_OF_HASH_MAP, (float)0.75);
+		//ageList = new LinkedList<Long>();
+		ageList = new ArrayList<Long>((int)MAX_SIZE_OF_HASH_MAP);
 		hashMapSize = 0;
 	}
 	
@@ -22,15 +28,35 @@ public class FixedSizeTranspositionTable {
 		return hashMap.containsKey(hashCode);
 	}
 	
+	private void moveToFrontOfAgeList(long hashCode) {
+		int index = ageList.indexOf(hashCode);
+		if (index != -1) {
+			ageList.remove(index);
+		}
+		//ageList.addFirst(hashCode);
+		ageList.add(0, hashCode);
+	}
+	
 	public Transposition getTransposition(long hashCode) {
-		return hashMap.get(hashCode);
+		Transposition retrievedTrans = hashMap.get(hashCode);
+		if (retrievedTrans != null) {
+			moveToFrontOfAgeList(hashCode);
+		}
+		return retrievedTrans;
 	}
 	
 	public void putTransposition(long hashCode, Transposition trans) {
-		if (hashMapSize >= MAX_SIZE_OF_HASH_MAP)
-			return;
+		if (hashMapSize >= MAX_SIZE_OF_HASH_MAP) {
+			// Remove the oldest hash to make way for this one
+			//Long hashToEject = ((LinkedList<Long>)ageList).removeLast();
+			Long hashToEject = ageList.remove((int)MAX_SIZE_OF_HASH_MAP-1);
+			hashMap.remove(hashToEject);
+			hashMapSize--;
+		}
 		if (hashMap.put(hashCode, trans) == null) {
+			// Only increment size if hash wasn't already contained, otherwise overwrites
 			hashMapSize++;
-		};
+		}
+		moveToFrontOfAgeList(hashCode);
 	}
 }
