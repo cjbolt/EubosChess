a9a33d61614c9275cca1254dbaa8278e28683278
Chris Bolt <c_j_bolt@yahoo.co.uk>
2019-10-25 17:18:20 +0100

    Optimise MoveList class

--- EubosChess/src/main/java/eubos/position/MoveList.java
+++ EubosChess/src/main/java/eubos/position/MoveList.java
@@ -2,7 +2,6 @@ package eubos.position;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -18,35 +17,36 @@ public class MoveList implements Iterable<GenericMove> {
 
 	public MoveList(PositionManager pm) {
 		partial = new ArrayList<GenericMove>();
+		all = new ArrayList<GenericMove>();
+		
 		Colour onMove = pm.getOnMove();
-		List<GenericMove> newMoveList = new LinkedList<GenericMove>();
-		Iterator<GenericMove> iter_ml = getRawList(pm).iterator();
 		int numCaptureOrCastleMoves = 0;
-		while ( iter_ml.hasNext() ) {
-			GenericMove currMove = iter_ml.next();
+		for (GenericMove currMove : getRawList(pm)) {
 			try {
 				pm.performMove(currMove);
-				// Scratch any moves resulting in the king being in check
-				if (pm.isKingInCheck(onMove))
-					iter_ml.remove();
-				// Groom the movelist so that the moves expected to be best are searched first.
-				// This is to get max benefit form alpha beta algorithm
-				else if (pm.lastMoveWasCaptureOrCastle() ) {
-					newMoveList.add(0, currMove);
+				if (pm.isKingInCheck(onMove)) {
+					// Scratch any moves resulting in the king being in check
+				}
+				// Order so that the moves expected to be best are searched first, to get max benefit from alpha beta algorithm
+				else if (pm.lastMoveWasCapture() ) {
+					all.add(0, currMove);
 					partial.add(currMove);
 					numCaptureOrCastleMoves++;
+				} else if (pm.lastMoveWasCaptureOrCastle() ) {
+					// Add castle move at start of list, but don't enter on partial list
+					all.add(0, currMove);
+					numCaptureOrCastleMoves++;
 				} else if (pm.isKingInCheck(Colour.getOpposite(onMove))) {
-					newMoveList.add(numCaptureOrCastleMoves, currMove);
+					all.add(numCaptureOrCastleMoves, currMove);
 					partial.add(currMove);
 				} else if (currMove.promotion != null) {
-					newMoveList.add(numCaptureOrCastleMoves, currMove);
+					all.add(numCaptureOrCastleMoves, currMove);
 					partial.add(currMove);
 				} else {
-					newMoveList.add(currMove);
+					all.add(currMove);
 				}
 				pm.unperformMove();
 			} catch(InvalidPieceException e) {
-				
 			}
 		}
 		/* Walk move list one last time re-ordering so that: 
@@ -57,23 +57,11 @@ public class MoveList implements Iterable<GenericMove> {
 		 * 	then checks
 		 * 	the normal moves
 		 */
-		all = new ArrayList<GenericMove>(newMoveList);
 	}
 	
 	public MoveList(PositionManager pm, GenericMove seedMove) {
 		this(pm);
 		seedMoveListOrder(seedMove);
-	}
-	
-	private List<GenericMove> getRawList(PositionManager pm) {
-		ArrayList<GenericMove> entireMoveList = new ArrayList<GenericMove>();
-		Iterator<Piece> iter_p = pm.getTheBoard().iterateColour(pm.getOnMove());
-		while ( iter_p.hasNext() ) {
-			Piece currPiece = iter_p.next();
-			entireMoveList.addAll( currPiece.generateMoves( pm.getTheBoard() ));
-		}
-		pm.castling.addCastlingMoves(entireMoveList);
-		return entireMoveList;
 	}
 
 	public Iterator<GenericMove> getIterator(boolean getCapturesChecksAndPromotions) {
@@ -84,30 +72,16 @@ public class MoveList implements Iterable<GenericMove> {
 		}
 	}
 	
-	@Override
-	public Iterator<GenericMove> iterator() {
-		return all.iterator();
-	}
-	
-	private Iterator<GenericMove> getCapturesChecksAndPromotionsIterator() {
-		return this.partial.iterator();
-	}
-	
 	public void adjustForBestMove(GenericMove best) {
 		all = createCopyWithBestMoveAtHead(all, best);
-		if (partial.indexOf(best) != -1) {
+		if (partial.contains(best)) {
 			partial = createCopyWithBestMoveAtHead(partial, best);
 		}
 	}
-
-	private ArrayList<GenericMove> createCopyWithBestMoveAtHead(List<GenericMove> listToReorder, GenericMove best) {
-		// It is done in this heavyweight fashion to avoid concurrent modification issues as we adjust the ml
-		// that we are currently iterating through in the ply searcher class.
-		LinkedList<GenericMove> ordered_ml = new LinkedList<GenericMove>();
-		ordered_ml.addAll(listToReorder);
-		ordered_ml.remove(best);
-		ordered_ml.add(0, best);
-		return new ArrayList<GenericMove>(ordered_ml);
+	
+	@Override
+	public Iterator<GenericMove> iterator() {
+		return all.iterator();
 	}
 	
 	public boolean isMateOccurred() {
@@ -128,10 +102,39 @@ public class MoveList implements Iterable<GenericMove> {
 		return this.all;
 	}
 	
+	private List<GenericMove> getRawList(PositionManager pm) {
+		ArrayList<GenericMove> entireMoveList = new ArrayList<GenericMove>();
+		Iterator<Piece> iter_p = pm.getTheBoard().iterateColour(pm.getOnMove());
+		while ( iter_p.hasNext() ) {
+			Piece currPiece = iter_p.next();
+			entireMoveList.addAll( currPiece.generateMoves( pm.getTheBoard() ));
+		}
+		pm.castling.addCastlingMoves(entireMoveList);
+		return entireMoveList;
+	}
+	
+	private Iterator<GenericMove> getCapturesChecksAndPromotionsIterator() {
+		return this.partial.iterator();
+	}
+
+	private ArrayList<GenericMove> createCopyWithBestMoveAtHead(List<GenericMove> listToReorder, GenericMove best) {
+		// It is done in this heavyweight fashion to avoid concurrent modification issues as we adjust the ml
+		// that we are currently iterating through in the ply searcher class.
+		ArrayList<GenericMove> ordered_ml = new ArrayList<GenericMove>(listToReorder);
+		ordered_ml.remove(best);
+		ordered_ml.add(0, best);
+		return ordered_ml;
+	}
+	
 	private void seedMoveListOrder(GenericMove prevBest) {
-		if (this.all.contains(prevBest)) {
-			this.all.remove(prevBest);
-			this.all.add(0,prevBest);
+		seedList(this.all, prevBest);
+		seedList(this.partial, prevBest);
+	}
+	
+	private void seedList(List<GenericMove> listToSeed, GenericMove prevBest) {
+		if (listToSeed.contains(prevBest)) {
+			listToSeed.remove(prevBest);
+			listToSeed.add(0,prevBest);
 		}
 	}
 }
