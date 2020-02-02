c8ff0a2fd8aa7210cf6676f432fc265c2c74e44e
Chris Bolt <c_j_bolt@yahoo.co.uk>
2020-02-02 19:58:12 +0000

    Small optimisation to move generation

--- EubosChess/src/main/java/eubos/board/Direction.java
+++ EubosChess/src/main/java/eubos/board/Direction.java
@@ -13,38 +13,42 @@ public enum Direction {
 		switch( dir ) {
 		case downLeft:
 		case leftDown:
-			if ( Position.getFile(startSq) != IntFile.Fa && Position.getRank(startSq) != IntRank.R1 )
+			if ( Position.getFile(startSq) > IntFile.Fa && 
+				 Position.getRank(startSq) > IntRank.R1 )
 				retVal = startSq-17;
 			break;
 		case down:
-			if ( Position.getRank(startSq) != IntRank.R1 )
+			if ( Position.getRank(startSq) > IntRank.R1 )
 				retVal = startSq-16;
 			break;
 		case downRight:
 		case rightDown:
-			if ( Position.getFile(startSq) != IntFile.Fh && Position.getRank(startSq) != IntRank.R1 )
+			if ( Position.getFile(startSq) < IntFile.Fh && 
+				 Position.getRank(startSq) > IntRank.R1 )
 				retVal = startSq-15;
 			break;
 		case left:
-			if ( Position.getFile(startSq) != IntFile.Fa )
+			if ( Position.getFile(startSq) > IntFile.Fa )
 				retVal = startSq-1;
 			break;
 		case right:
-			if ( Position.getFile(startSq) != IntFile.Fh )
+			if ( Position.getFile(startSq) < IntFile.Fh )
 				retVal = startSq+1;
 			break;
 		case up:
-			if ( Position.getRank(startSq) != IntRank.R8 )
+			if ( Position.getRank(startSq) < IntRank.R8 )
 				retVal = startSq+16;
 			break;
 		case upLeft:
 		case leftUp:
-			if ( Position.getFile(startSq) != IntFile.Fa && Position.getRank(startSq) != IntRank.R8 )
+			if ( Position.getFile(startSq) > IntFile.Fa && 
+				 Position.getRank(startSq) < IntRank.R8 )
 				retVal = startSq+15;
 			break;
 		case upRight:
 		case rightUp:
-			if ( Position.getFile(startSq) != IntFile.Fh && Position.getRank(startSq) != IntRank.R8 )
+			if ( Position.getFile(startSq) < IntFile.Fh &&
+				 Position.getRank(startSq) < IntRank.R8 )
 				retVal = startSq+17;
 			break;
 		default:
@@ -57,58 +61,50 @@ public enum Direction {
 		int retVal = Position.NOPOSITION;
 		switch( dir ) {
 		case downLeft:
-			if ( Position.getFile(onSquare) != IntFile.Fa && 
-			     Position.getRank(onSquare) != IntRank.R1 &&
-			     Position.getRank(onSquare) != IntRank.R2) {
+			if ( Position.getFile(onSquare) > IntFile.Fa && 
+			     Position.getRank(onSquare) > IntRank.R2) {
 				retVal = onSquare-33;
 			}
 			break;
 		case downRight:
-			if ( Position.getFile(onSquare) != IntFile.Fh && 
-			     Position.getRank(onSquare) != IntRank.R1 &&
-		         Position.getRank(onSquare) != IntRank.R2) {
+			if ( Position.getFile(onSquare) < IntFile.Fh && 
+			     Position.getRank(onSquare) > IntRank.R2) {
 				retVal = onSquare-31;
 			}
 			break;
 		case leftUp:
-			if (Position.getFile(onSquare) != IntFile.Fa && 
-				Position.getFile(onSquare) != IntFile.Fb && 
-				Position.getRank(onSquare) != IntRank.R8 )  {
+			if (Position.getFile(onSquare) > IntFile.Fb && 
+				Position.getRank(onSquare) < IntRank.R8 )  {
 				retVal = onSquare+14;
 			}
 			break;
 		case leftDown:
-			if (Position.getFile(onSquare) != IntFile.Fa && 
-				Position.getFile(onSquare) != IntFile.Fb && 
-				Position.getRank(onSquare) != IntRank.R1 )  {
+			if (Position.getFile(onSquare) > IntFile.Fb && 
+				Position.getRank(onSquare) > IntRank.R1 )  {
 				retVal = onSquare-18;
 			}
 			break;
 		case rightUp:
-			if (Position.getFile(onSquare) != IntFile.Fg && 
-				Position.getFile(onSquare) != IntFile.Fh && 
-				Position.getRank(onSquare) != IntRank.R8 ) {
+			if (Position.getFile(onSquare) < IntFile.Fg && 
+				Position.getRank(onSquare) < IntRank.R8 ) {
 				retVal = onSquare+18;
 			}
 			break;
 		case rightDown:
-			if (Position.getFile(onSquare) != IntFile.Fg && 
-				Position.getFile(onSquare) != IntFile.Fh && 
-				Position.getRank(onSquare) != IntRank.R1 ) {
+			if (Position.getFile(onSquare) < IntFile.Fg && 
+				Position.getRank(onSquare) > IntRank.R1 ) {
 				retVal = onSquare-14;
 			}
 			break;
 		case upLeft:
-			if (Position.getFile(onSquare) != IntFile.Fa && 
-				Position.getRank(onSquare) != IntRank.R8 &&
-			    Position.getRank(onSquare) != IntRank.R7) {
+			if (Position.getFile(onSquare) > IntFile.Fa && 
+				Position.getRank(onSquare) < IntRank.R7) {
 				retVal = onSquare+31;
 			}
 			break;
 		case upRight:
-			if (Position.getFile(onSquare) != IntFile.Fh && 
-				Position.getRank(onSquare) != IntRank.R8 &&
-				Position.getRank(onSquare) != IntRank.R7) {
+			if (Position.getFile(onSquare) < IntFile.Fh && 
+				Position.getRank(onSquare) < IntRank.R7) {
 				retVal = onSquare+33;	
 			}
 			break;
