a5eb03b94e4a96d90145005c33fc445958348149
Chris Bolt <c_j_bolt@yahoo.co.uk>
2020-09-27 15:04:25 +0100

    Add KQk mate in seven test.

--- EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
+++ EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
@@ -299,6 +299,34 @@ public class EubosEngineMainTest {
 	}
 	
 	@Test
+	public void test_KQk_mate_in_7() throws InterruptedException, IOException {
+		setupEngine();
+		// 1
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"11"+CMD_TERMINATOR,BEST_PREFIX+"f8b4"+CMD_TERMINATOR));
+		// 2
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113 moves f8b4 d5c6"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"6"+CMD_TERMINATOR,BEST_PREFIX+"g7f7"+CMD_TERMINATOR));
+		// 3
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113 moves f8b4 d5c6 g7f7 c6d5"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"6"+CMD_TERMINATOR,BEST_PREFIX+"f7f6"+CMD_TERMINATOR));
+		// 4
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113 moves f8b4 d5c6 g7f7 c6d5 f7f6 d5c6"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"6"+CMD_TERMINATOR,BEST_PREFIX+"f6e6"+CMD_TERMINATOR));
+		// 5
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113 moves f8b4 d5c6 g7f7 c6d5 f7f6 d5c6 f6e6 c6c7"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"6"+CMD_TERMINATOR,BEST_PREFIX+"b4b5"+CMD_TERMINATOR));
+		// 6
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113 moves f8b4 d5c6 g7f7 c6d5 f7f6 d5c6 f6e6 c6c7 b4b5 c7c8"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"6"+CMD_TERMINATOR,BEST_PREFIX+"e6d6"+CMD_TERMINATOR));
+		// 7
+		commands.add(new commandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113 moves f8b4 d5c6 g7f7 c6d5 f7f6 d5c6 f6e6 c6c7 b4b5 c7c8 e6d6 c8d8"+CMD_TERMINATOR, null));
+		commands.add(new commandPair(GO_DEPTH_PREFIX+"6"+CMD_TERMINATOR,BEST_PREFIX+"b5d7"+CMD_TERMINATOR));
+		performTest(8000);
+		assertEquals(14, (int)classUnderTest.dc.getNumEntries());
+	}
+	
+	@Test
 	public void test_createPositionFromAnalyseCommand() throws IllegalNotationException {
 		// Black move 62
 		ArrayList<GenericMove> applyMoveList = new ArrayList<GenericMove>();
