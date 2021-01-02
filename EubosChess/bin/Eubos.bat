ef741543d920f2a07aa13b1cf49caf0c50869be5
Chris Bolt <c_j_bolt@yahoo.co.uk>
2021-01-02 18:03:34 +0000

    Add JIT watch required switches to batch file

--- EubosChess/bin/Eubos.bat
+++ EubosChess/bin/Eubos.bat
@@ -1,3 +1,4 @@
 @echo off
 REM This batch file is to start Eubos as a process from e.g. Arena, use the UCI protocol via stdin/stdout to communicate with Eubos
-java -Xshare:off -jar .\Eubos.jar
+REM java -XX:+UnlockDiagnosticVMOptions -Xshare:off -XX:+LogCompilation -XX:+TraceClassLoading -Djdk.attach.allowAttachSelf -Xmx4G -Xms4G -jar .\Eubos.jar
+java -Xshare:off -Djdk.attach.allowAttachSelf -Xmx4G -Xms4G -jar .\Eubos.jar
