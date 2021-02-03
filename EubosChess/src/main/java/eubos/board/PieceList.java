a3df775f05aeb11e99d4d2e8da9a427b112d8096
cjbolt <c_j_bolt@yahoo.co.uk>
2021-02-03 17:09:23 +0000

    Small improvements in PieceList implementation. trade mem for speed.

--- EubosChess/src/main/java/eubos/board/PieceList.java
+++ EubosChess/src/main/java/eubos/board/PieceList.java
@@ -17,47 +17,39 @@ public class PieceList {
 	// 2nd index is piece type
 	// 3rd index is index into that piece array, gives the position of that piece
 	//     NOTE: DELIBERATELY TOO LONG, simplifies array traversal when removing and bringing down pieces
-	private int [][][] piece_list = new int[NUM_COLOURS][NUM_PIECE_TYPES][MAX_NUM_PIECES+1];
+	private int [][] piece_list = new int[NUM_COLOURS*(NUM_PIECE_TYPES+1)][MAX_NUM_PIECES+1];
 	
 	private Board theBoard;
 	
 	public PieceList(Board theBoard) {
-		for (int [][] side : piece_list) {
-			for ( int[] piece_type : side) {
-				Arrays.fill(piece_type, Position.NOPOSITION);
-			}
+		for (int [] piece : piece_list) {
+			Arrays.fill(piece, Position.NOPOSITION);
 		}
 		this.theBoard = theBoard;
 	}
 	
 	public void addPiece(int piece, int atPos) {
-		int colour_index = piece >> Piece.COLOUR_BIT_SHIFT;
-		int piece_index = piece & Piece.PIECE_NO_COLOUR_MASK;
-		
 		if (EubosEngineMain.ASSERTS_ENABLED) {
 			assert piece != Piece.NONE;
 			assert atPos != Position.NOPOSITION;
 		}
 		
 		for (int piece_number = 0; piece_number < MAX_NUM_PIECES; piece_number++) {
-			if (piece_list[colour_index][piece_index][piece_number] == Position.NOPOSITION) {
-				piece_list[colour_index][piece_index][piece_number] = atPos;
+			if (piece_list[piece][piece_number] == Position.NOPOSITION) {
+				piece_list[piece][piece_number] = atPos;
 				break;
 			}
 		}
 	}
 	
 	public void removePiece(int piece, int atPos) {
-		int colour_index = piece >> Piece.COLOUR_BIT_SHIFT;
-		int piece_index = piece & Piece.PIECE_NO_COLOUR_MASK;
-		
 		if (EubosEngineMain.ASSERTS_ENABLED) {
 			assert piece != Piece.NONE;
 			assert atPos != Position.NOPOSITION;
 		}
 		
 		// find the index into the relevant piece list to clear
-		int [] the_list = piece_list[colour_index][piece_index];
+		int [] the_list = piece_list[piece];
 		boolean found = false;
 		for (int i=0; i < MAX_NUM_PIECES; i++) {
 			if (the_list[i] == atPos) {
@@ -79,9 +71,6 @@ public class PieceList {
 	}
 	
 	public void updatePiece(int piece, int atPos, int targetPos) {
-		int colour_index = piece >> Piece.COLOUR_BIT_SHIFT;
-		int piece_index = piece & Piece.PIECE_NO_COLOUR_MASK;
-		
 		if (EubosEngineMain.ASSERTS_ENABLED) {
 			assert piece != Piece.NONE;
 			assert atPos != Position.NOPOSITION;
@@ -89,20 +78,16 @@ public class PieceList {
 		
 		// find the piece to update
 		for (int piece_number = 0; piece_number < MAX_NUM_PIECES; piece_number++) {
-			if (piece_list[colour_index][piece_index][piece_number] == atPos) {
-				piece_list[colour_index][piece_index][piece_number] = targetPos;
+			if (piece_list[piece][piece_number] == atPos) {
+				piece_list[piece][piece_number] = targetPos;
 				break;
 			}
 		}
 	}
 	
 	public void updatePiece(int oldPiece, int piece, int atPos, int targetPos) {
-		int colour_index = piece >> Piece.COLOUR_BIT_SHIFT;
-		int piece_index = piece & Piece.PIECE_NO_COLOUR_MASK;
-		int old_piece_index = oldPiece & Piece.PIECE_NO_COLOUR_MASK;
-		
 		// find the index into the relevant piece list to clear
-		int [] the_list = piece_list[colour_index][old_piece_index];
+		int [] the_list = piece_list[oldPiece];
 		boolean found = false;
 		for (int i=0; i < MAX_NUM_PIECES; i++) {
 			if (the_list[i] == atPos) {
@@ -121,8 +106,8 @@ public class PieceList {
 		}
 		// find the piece to update
 		for (int piece_number = 0; piece_number < MAX_NUM_PIECES; piece_number++) {
-			if (piece_list[colour_index][piece_index][piece_number] == Position.NOPOSITION) {
-				piece_list[colour_index][piece_index][piece_number] = targetPos;
+			if (piece_list[piece][piece_number] == Position.NOPOSITION) {
+				piece_list[piece][piece_number] = targetPos;
 				break;
 			}
 		}
@@ -130,21 +115,18 @@ public class PieceList {
 	
 	public void forEachPieceDoCallback(IForEachPieceCallback caller) {
 		// white pieces
-		int currPiece = Piece.KING;
-		helper(piece_list[0], caller, currPiece);
+		forEachPieceTypeOfSideHelper(caller, Piece.WHITE_KING);
 		// black pieces
-		currPiece = Piece.BLACK_KING;
-		helper(piece_list[1], caller, currPiece);
+		forEachPieceTypeOfSideHelper(caller, Piece.BLACK_KING);
 	}
 	
-	private void helper(int [][] side, IForEachPieceCallback caller, int currPiece) {
-		for (int i = Piece.KING; i < NUM_PIECE_TYPES; i++) {
-			anotherHelper(side[i], caller, currPiece);
-			currPiece++;
+	private void forEachPieceTypeOfSideHelper(IForEachPieceCallback caller, int currPiece) {
+		for (int piece = currPiece; piece < currPiece+NUM_PIECE_TYPES; piece++) {
+			forEachPieceOfTypeHelper(piece_list[piece], caller, piece);
 		}
 	}
 	
-	private void anotherHelper(int [] piece_array, IForEachPieceCallback caller, int piece) {
+	private void forEachPieceOfTypeHelper(int [] piece_array, IForEachPieceCallback caller, int piece) {
 		for (int atPos : piece_array) {
 			if (atPos != Position.NOPOSITION) {
 				caller.callback(piece, atPos);
@@ -153,34 +135,34 @@ public class PieceList {
 	}
 	
 	public void addMovesEndgame(boolean ownSideIsWhite, List<Integer> movesList) {
-		int [][] side = piece_list[ownSideIsWhite ? 0 : 1];
+		int side = ownSideIsWhite ? 0 : Piece.BLACK;
 		{
-			int atSquare = side[Piece.KING][0];
+			int atSquare = piece_list[side+Piece.KING][0];
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.king_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			}
 		}
-		for(int atSquare : side[Piece.QUEEN]) {
+		for(int atSquare : piece_list[side+Piece.QUEEN]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.queen_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.ROOK]) {
+		for(int atSquare : piece_list[side+Piece.ROOK]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.rook_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.BISHOP]) {
+		for(int atSquare : piece_list[side+Piece.BISHOP]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.bishop_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.KNIGHT]) {
+		for(int atSquare : piece_list[side+Piece.KNIGHT]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.knight_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.PAWN]) {
+		for(int atSquare : piece_list[side+Piece.PAWN]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.pawn_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
@@ -188,34 +170,34 @@ public class PieceList {
 	}
 	
 	public void addMovesMiddlegame(boolean ownSideIsWhite, List<Integer> movesList) {
-		int [][] side = piece_list[ownSideIsWhite ? 0 : 1];
-		for(int atSquare : side[Piece.QUEEN]) {
+		int side = ownSideIsWhite ? 0 : Piece.BLACK;
+		for(int atSquare : piece_list[side+Piece.QUEEN]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.queen_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.ROOK]) {
+		for(int atSquare : piece_list[side+Piece.ROOK]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.rook_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.BISHOP]) {
+		for(int atSquare : piece_list[side+Piece.BISHOP]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.bishop_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.KNIGHT]) {
+		for(int atSquare : piece_list[side+Piece.KNIGHT]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.knight_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
-		for(int atSquare : side[Piece.PAWN]) {
+		for(int atSquare : piece_list[side+Piece.PAWN]) {
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.pawn_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			} else break;
 		}
 		{
-			int atSquare = side[Piece.KING][0];
+			int atSquare = piece_list[side+Piece.KING][0];
 			if (atSquare != Position.NOPOSITION) {			
 				Piece.king_generateMoves(movesList, theBoard, atSquare, ownSideIsWhite);
 			}
@@ -223,62 +205,57 @@ public class PieceList {
 	}
 	
 	public void evaluateMaterialBalanceAndPieceMobility(boolean isWhite) {
-		int [][] side = piece_list[isWhite ? 0 : 1];
-		//boolean isWhite = true;
-		//for (int [][] side : piece_list) {
-			{
-				int atSquare = side[Piece.KING][0];
-				if (atSquare != Position.NOPOSITION) {			
-					theBoard.me.addPiece(isWhite, Piece.KING);
-					theBoard.me.addPosition(isWhite, theBoard.isEndgame ? Board.KING_ENDGAME_WEIGHTINGS[atSquare] : Board.KING_MIDGAME_WEIGHTINGS[atSquare]);
-				}
+		int side = isWhite ? 0 : Piece.BLACK;
+		{
+			int atSquare = piece_list[side+Piece.KING][0];
+			if (atSquare != Position.NOPOSITION) {			
+				theBoard.me.addPiece(isWhite, Piece.KING);
+				theBoard.me.addPosition(isWhite, theBoard.isEndgame ? Board.KING_ENDGAME_WEIGHTINGS[atSquare] : Board.KING_MIDGAME_WEIGHTINGS[atSquare]);
 			}
-			for(int atSquare : side[Piece.QUEEN]) {
-				if (atSquare != Position.NOPOSITION) {			
-					theBoard.me.addPiece(isWhite, Piece.QUEEN);
-					if (!theBoard.isEndgame)
-						theBoard.me.addPosition(isWhite, (byte)(theBoard.getNumEmptyAllDirectSquares(atSquare)*2));
-				} else break;
-			}
-			for(int atSquare : side[Piece.ROOK]) {
-				if (atSquare != Position.NOPOSITION) {			
-					theBoard.me.addPiece(isWhite, Piece.ROOK);
-					if (!theBoard.isEndgame)
-						theBoard.me.addPosition(isWhite, (byte)(theBoard.getNumEmptyRankFileSquares(atSquare)*2));
-				} else break;
-			}
-			for(int atSquare : side[Piece.BISHOP]) {
-				if (atSquare != Position.NOPOSITION) {			
-					theBoard.me.addPiece(isWhite, Piece.BISHOP);
-					if (!theBoard.isEndgame)
-						theBoard.me.addPosition(isWhite, (byte)(theBoard.getNumEmptyDiagonalSquares(atSquare)*2));
-				} else break;
-			}
-			for(int atSquare : side[Piece.KNIGHT]) {
-				if (atSquare != Position.NOPOSITION) {			
-					theBoard.me.addPiece(isWhite, Piece.KNIGHT);
-					theBoard.me.addPosition(isWhite, Board.KNIGHT_WEIGHTINGS[atSquare]);
-				} else break;
-			}
-			for(int atSquare : side[Piece.PAWN]) {
-				if (atSquare != Position.NOPOSITION) {			
-					theBoard.me.addPiece(isWhite, Piece.PAWN);
-					theBoard.me.addPosition(isWhite, isWhite ? Board.PAWN_WHITE_WEIGHTINGS[atSquare] : Board.PAWN_BLACK_WEIGHTINGS[atSquare]);
-				} else break;
-			}
-			//isWhite = !isWhite;
-		//}
+		}
+		for(int atSquare : piece_list[side+Piece.QUEEN]) {
+			if (atSquare != Position.NOPOSITION) {			
+				theBoard.me.addPiece(isWhite, Piece.QUEEN);
+				if (!theBoard.isEndgame)
+					theBoard.me.addPosition(isWhite, (byte)(theBoard.getNumEmptyAllDirectSquares(atSquare)*2));
+			} else break;
+		}
+		for(int atSquare : piece_list[side+Piece.ROOK]) {
+			if (atSquare != Position.NOPOSITION) {			
+				theBoard.me.addPiece(isWhite, Piece.ROOK);
+				if (!theBoard.isEndgame)
+					theBoard.me.addPosition(isWhite, (byte)(theBoard.getNumEmptyRankFileSquares(atSquare)*2));
+			} else break;
+		}
+		for(int atSquare : piece_list[side+Piece.BISHOP]) {
+			if (atSquare != Position.NOPOSITION) {			
+				theBoard.me.addPiece(isWhite, Piece.BISHOP);
+				if (!theBoard.isEndgame)
+					theBoard.me.addPosition(isWhite, (byte)(theBoard.getNumEmptyDiagonalSquares(atSquare)*2));
+			} else break;
+		}
+		for(int atSquare : piece_list[side+Piece.KNIGHT]) {
+			if (atSquare != Position.NOPOSITION) {			
+				theBoard.me.addPiece(isWhite, Piece.KNIGHT);
+				theBoard.me.addPosition(isWhite, Board.KNIGHT_WEIGHTINGS[atSquare]);
+			} else break;
+		}
+		for(int atSquare : piece_list[side+Piece.PAWN]) {
+			if (atSquare != Position.NOPOSITION) {			
+				theBoard.me.addPiece(isWhite, Piece.PAWN);
+				theBoard.me.addPosition(isWhite, isWhite ? Board.PAWN_WHITE_WEIGHTINGS[atSquare] : Board.PAWN_BLACK_WEIGHTINGS[atSquare]);
+			} else break;
+		}
 	}
 	
 	public void forEachPawnOfSideDoCallback(IForEachPieceCallback caller, boolean sideIsBlack) {
-		int colour_index = sideIsBlack ? 1 : 0;
 		int pawn = sideIsBlack ? Piece.BLACK_PAWN : Piece.WHITE_PAWN;
-		int [] pawns = piece_list[colour_index][Piece.PAWN];
-		anotherHelper(pawns, caller, pawn);		
+		int [] pawns = piece_list[pawn];
+		forEachPieceOfTypeHelper(pawns, caller, pawn);		
 	}
 	
 	public int getKingPos(boolean sideIsWhite) {
-		int colour_index = sideIsWhite ? 0 : 1;
-		return piece_list[colour_index][Piece.KING][0];
+		int piece = sideIsWhite ? Piece.WHITE_KING : Piece.BLACK_KING;
+		return piece_list[piece][0];
 	}
 }
