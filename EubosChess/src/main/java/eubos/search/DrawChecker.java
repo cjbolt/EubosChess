8a00e98b5c5b49586aba5c3dc6749dba402c43ca
cjbolt <c_j_bolt@yahoo.co.uk>
2022-01-11 11:29:05 +0000

    Optimise draw checking at ply.

--- EubosChess/src/main/java/eubos/search/DrawChecker.java
+++ EubosChess/src/main/java/eubos/search/DrawChecker.java
@@ -36,7 +36,9 @@ public class DrawChecker {
 	}
 	
 	private boolean isPositionReachedBefore(long posHash, int currentPly) {
-		for (int i=checkFromPly; i < currentPly; i++ ) {
+		// Only search previous positions at odd/even ply depth, don't need to check if not same on move. 
+		int offset = (checkFromPly & 0x1) != (currentPly & 0x1) ? 1 : 0;
+		for (int i=checkFromPly+offset; i < currentPly; i+=2 ) {
 			if (reachedPositions[i] == posHash)
 				return true;
 		}
