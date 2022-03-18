3da928a7c53f52c52952038b623a074d5be16f15
chris <c_j_bolt@yahoo.co.uk>
2022-03-18 21:22:32 +0000

    Prepare for v2.10 release

--- EubosChess/src/main/java/eubos/main/EubosEngineMain.java
+++ EubosChess/src/main/java/eubos/main/EubosEngineMain.java
@@ -52,7 +52,7 @@ public class EubosEngineMain extends AbstractEngine {
 	public static final byte SEARCH_DEPTH_IN_PLY = Byte.MAX_VALUE;
 	public static final int DEFAULT_NUM_SEARCH_THREADS = 1;
 	
-	public static final boolean ENABLE_LOGGING = true;
+	public static final boolean ENABLE_LOGGING = false;
 	public static final boolean ENABLE_UCI_INFO_SENDING = true;
 	public static final boolean ENABLE_UCI_MOVE_NUMBER = false;
 	
