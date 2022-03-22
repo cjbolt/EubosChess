40a8cc1673fbcfd8bdcac8dfe45590e71229988b
cjbolt <c_j_bolt@yahoo.co.uk>
2022-03-22 20:53:25 +0000

    Remove fiddling with the hash map size that was only needed when allocating Transposition objects. Account for Zobrist in memory used.

--- EubosChess/src/main/java/eubos/search/transposition/FixedSizeTranspositionTable.java
+++ EubosChess/src/main/java/eubos/search/transposition/FixedSizeTranspositionTable.java
@@ -10,31 +10,24 @@ import it.unimi.dsi.fastutil.longs.Long2LongMap;
 public class FixedSizeTranspositionTable {
 	
 	private static final boolean DEBUG_LOGGING = false;
-	
-	public static final long ELEMENTS_DEFAULT_HASH_SIZE = (1L << 25);
-	
-	public static final long BYTES_TRANSPOSTION_ELEMENT;
-	static {
-		BYTES_TRANSPOSTION_ELEMENT = 8L;
-	}
-	
+
 	public static final long BYTES_HASHMAP_ENTRY;
 	static {
 		BYTES_HASHMAP_ENTRY = ClassLayout.parseClass(Long2LongMap.Entry.class).instanceSize();
 	}
 	
 	public static final long BYTES_HASHMAP_ZOBRIST_KEY = 8L;
-	
-	public static final long BYTES_PER_TRANSPOSITION =  BYTES_TRANSPOSTION_ELEMENT + BYTES_HASHMAP_ENTRY; // + BYTES_HASHMAP_ZOBRIST_KEY;
+	public static final long BYTES_TRANSPOSTION_ELEMENT = 8L;
+	public static final long BYTES_PER_TRANSPOSITION =
+			BYTES_TRANSPOSTION_ELEMENT + BYTES_HASHMAP_ENTRY + BYTES_HASHMAP_ZOBRIST_KEY;
 	
 	public static final long BYTES_PER_MEGABYTE = (1024L * 1000L);
 	
-	//public static final long MBYTES_DEFAULT_HASH_SIZE = (ELEMENTS_DEFAULT_HASH_SIZE*BYTES_PER_TRANSPOSITION)/BYTES_PER_MEGABYTE;
 	public static final long MBYTES_DEFAULT_HASH_SIZE = 256L;
 			
 	private Long2LongLinkedOpenHashMap hashMap = null;
 	private long hashMapSize = 0;
-	private long maxHashMapSize = ELEMENTS_DEFAULT_HASH_SIZE;
+	private long maxHashMapSize = 0;
 	
 	public FixedSizeTranspositionTable() {
 		this(MBYTES_DEFAULT_HASH_SIZE, 1);
@@ -42,22 +35,12 @@ public class FixedSizeTranspositionTable {
 	
 	public FixedSizeTranspositionTable(long hashSizeMBytes, int numThreads) {
 		long hashSizeElements = (hashSizeMBytes * BYTES_PER_MEGABYTE) / BYTES_PER_TRANSPOSITION;
-		long maxHeapSize = Runtime.getRuntime().maxMemory();
-		if ((hashSizeMBytes * BYTES_PER_MEGABYTE) > ((maxHeapSize*4)/10)) {
-			/* If the configured hash size is greater than 40% of the heap, then reduce the hash size
-			 * as we are resource constrained and garbage collection will kill speed of the engine. */
-			hashSizeElements = ((maxHeapSize*4)/10) / BYTES_PER_TRANSPOSITION;
-		}
 		
 		if (DEBUG_LOGGING) {
+			EubosEngineMain.logger.info(String.format("BYTES_PER_TRANSPOSITION=%d", BYTES_PER_TRANSPOSITION));
 			EubosEngineMain.logger.info(String.format(
-					"BYTES_TRANSPOSTION_ELEMENT=%d, BYTES_PER_TRANSPOSITION=%d", 
-					BYTES_TRANSPOSTION_ELEMENT, BYTES_PER_TRANSPOSITION));
-			
-			EubosEngineMain.logger.info(String.format(
-					"Hash dimensions requestedSizeMBytes=%d maxHeapSizeMBytes=%d, maxSizeElements=%d, maxSizeMBytes=%d", 
-					hashSizeMBytes, maxHeapSize/BYTES_PER_MEGABYTE, hashSizeElements,
-					(hashSizeElements*BYTES_PER_TRANSPOSITION)/BYTES_PER_MEGABYTE));
+					"Hash dimensions requestedSizeMBytes=%d BYTES_PER_TRANSPOSITION=%d, maxSizeElements=%d", 
+					hashSizeMBytes, BYTES_PER_TRANSPOSITION, hashSizeElements));
 		}
 
 		hashMap = new Long2LongLinkedOpenHashMap((int)hashSizeElements);
@@ -81,6 +64,6 @@ public class FixedSizeTranspositionTable {
 	}
 	
 	public synchronized short getHashUtilisation() {
-		return (short) (( ((long) hashMapSize)*(long)1000) / maxHashMapSize);
+		return (short) ((hashMapSize*1000L)/maxHashMapSize);
 	}	
 }
