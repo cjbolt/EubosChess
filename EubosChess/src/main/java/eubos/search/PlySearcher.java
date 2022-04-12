ea64d8e1a37fd16fddcf7f941e337a6da8115d46
chris <c_j_bolt@yahoo.co.uk>
2022-04-12 08:55:34 +0100

    Remove assert that is no longer valid

--- EubosChess/src/main/java/eubos/search/PlySearcher.java
+++ EubosChess/src/main/java/eubos/search/PlySearcher.java
@@ -434,7 +434,6 @@ public class PlySearcher {
 				moveNumber += 1;
 				if (EubosEngineMain.ENABLE_ASSERTS) {
 					assert currMove != Move.NULL_MOVE: "Null move found in MoveList";
-					//assert moveNumber <= ml.getList(currPly).size() : "MoveList is too long";
 				}
 				if (moveNumber == 1) {
 					pc.initialise(currPly, currMove);
