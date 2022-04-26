cjbolt/EubosChess

commit 45a0e23d9bdd0c99c22cd6d30b16b8c012974d59

Store principal continuations in static arrays rather than lists, to reduce memory overhead.

EubosChess/src/main/java/eubos/search/PlySearcher.java
@@ -2,6 +2,7 @@ package eubos.search;
 
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.Arrays;
 
 import eubos.board.Piece;
@@ -83,7 +84,7 @@ public class PlySearcher {
 	
 	private volatile boolean terminate = false;
 	
-	private List<Integer> lastPc;
+	private int[] lastPc;
 	private ITranspositionAccessor tt;
 	private SearchMetricsReporter sr;
 	private KillerList killers;
@@ -218,7 +219,7 @@ public class PlySearcher {
 		
 		// This move is only valid for the principal continuation, for the rest of the search, it is invalid. It can also be misleading in iterative deepening?
 		// It will deviate from the hash move when we start updating the hash during iterative deepening.
-		prevBestMove[0] = ((lastPc != null) && (lastPc.size() > 0)) ? lastPc.get(0) : Move.NULL_MOVE;
+		prevBestMove[0] = ((lastPc != null) && (lastPc.length > 0)) ? lastPc[0] : Move.NULL_MOVE;
 		prevBestMove[0] =  Move.clearBest(prevBestMove[0]);
 		if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) pc.clearContinuationBeyondPly(0);
 		
@@ -237,7 +238,7 @@ public class PlySearcher {
 		if (trans != 0L) {
 			evaluateTransposition(trans, depth);
 			if (isCutOff[0]) {
-				sm.setPrincipalVariationDataFromHash(0, pc.toPvList(0), (short)hashScore[0]);
+				sm.setPrincipalVariationDataFromHash(0, (short)hashScore[0]);
 				sr.reportPrincipalVariation(sm);
 				return hashScore[0];
 			}
@@ -347,7 +348,7 @@ public class PlySearcher {
 						
 		// This move is only valid for the principal continuation, for the rest of the search, it is invalid. It can also be misleading in iterative deepening?
 		// It will deviate from the hash move when we start updating the hash during iterative deepening.
-		prevBestMove[currPly] = ((lastPc != null) && (lastPc.size() > currPly)) ? lastPc.get(currPly) : Move.NULL_MOVE;
+		prevBestMove[currPly] = ((lastPc != null) && (lastPc.length > currPly)) ? lastPc[currPly] : Move.NULL_MOVE;
 		prevBestMove[currPly] =  Move.clearBest(prevBestMove[currPly]);
 		if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) pc.clearContinuationBeyondPly(currPly);
 		
@@ -721,7 +722,8 @@ public class PlySearcher {
 
 	private void reportPv(short positionScore) {
 		if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) {
-			sm.setPrincipalVariationData(extendedSearchDeepestPly, pc.toPvList(0), positionScore);
+			List<Integer> list = Arrays.stream(pc.toPvList(0)).boxed().collect(Collectors.toList());
+			sm.setPrincipalVariationData(extendedSearchDeepestPly, list, positionScore);
 			sr.reportPrincipalVariation(sm);
 			extendedSearchDeepestPly = 0;
 		}

EubosChess/src/main/java/eubos/search/PrincipalContinuation.java
@@ -1,35 +1,36 @@
 package eubos.search;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import eubos.main.EubosEngineMain;
 import eubos.position.Move;
+import it.unimi.dsi.fastutil.ints.IntArrays;
 
 public class PrincipalContinuation {
 
-	private List<List<Integer>> pc;
+	private int [][] pc;
+	private int [] length;
 	private SearchDebugAgent sda;
 
 	public PrincipalContinuation(int searchDepth, SearchDebugAgent sda) {
-		pc = new ArrayList<List<Integer>>(searchDepth);
-		for (int i=0; i<searchDepth; i++) {
-			pc.add(new ArrayList<Integer>(searchDepth));
+		// Create the pc list at each ply
+		pc = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY][];
+		for (int i = 0; i < EubosEngineMain.SEARCH_DEPTH_IN_PLY; i++) {
+			pc[i] = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY];
 		}
+		length = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY];
 		this.sda = sda;
 	}
 	
 	public int getBestMove(byte currPly) {
-		if (currPly < pc.size()) {
-			List<Integer> plyList = pc.get(currPly);
-			if (!plyList.isEmpty())
-				return plyList.get(0);
+		if (currPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			if (length[currPly] != 0)
+				return pc[currPly][0];
 		}
 		return Move.NULL_MOVE;
 	}
 	
 	public String toString() {
 		StringBuilder output = new StringBuilder();
-		for (int currPly = 0; currPly < pc.size(); currPly++) {
+		for (int currPly = 0; currPly < length[0]; currPly++) {
 			output.append(String.format("Ply %d (%s) ,", currPly, toStringAt(currPly)));
 		}
 		return output.toString();
@@ -37,12 +38,14 @@ public class PrincipalContinuation {
 
 	public String toStringAt(int currPly) {
 		StringBuilder output = new StringBuilder();
-		if (currPly < pc.size()) {
-			List<Integer> plyList = pc.get(currPly);
-			if (!plyList.isEmpty()) {
-				for (int currMove : plyList) {
-					//assert currMove != Move.NULL_MOVE;
-					output.append((Move.toString(currMove)));
+		if (currPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			if (length[currPly] != 0) {
+				for (int i=0; i<length[currPly]; i++) {
+					int currMove = pc[currPly][i];
+					if (EubosEngineMain.ENABLE_ASSERTS) {
+						assert currMove != Move.NULL_MOVE;
+					}
+					output.append(Move.toString(currMove));
 					output.append(' ');
 				}
 			}
@@ -50,46 +53,33 @@ public class PrincipalContinuation {
 		return output.toString();
 	}
 	
-	public static String pvToString(List<Integer> pv) {
-		StringBuilder output = new StringBuilder();
-		if (pv != null && !pv.isEmpty()) {
-			for (int currMove : pv) {
-				//assert currMove != Move.NULL_MOVE;
-				output.append((Move.toString(currMove)));
-				output.append(' ');
-			}
+	public int [] toPvList(int currPly) { 
+		if (currPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			int [] pv = IntArrays.trim(pc[currPly], length[currPly]);
+			return pv;
 		}
-		return output.toString();
-	}
-	
-	public List<Integer> toPvList(int currPly) { 
-		List<Integer> mv = new ArrayList<Integer>(pc.size());
-		if (currPly < pc.size()) {
-			for (int currMove : pc.get(currPly)) {
-				mv.add(currMove);
-			}
-		}
-		return mv;
+		return null;
 	}
 	
 	void initialise(int currPly, int currMove) {
-		if (currPly < pc.size()) {
-			List<Integer> plyToUpdatePc = pc.get(currPly);
-			plyToUpdatePc.clear();
-			plyToUpdatePc.add(currMove);
+		if (currPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			length[currPly] = 1;
+			pc[currPly][0] = currMove;
 		}
 	}
 	
 	// Bring down a pv from node further down the tree, with curr move added at the head
 	void update(int currPly, int currMove) {
-		if (currPly < pc.size()) {
-			List<Integer> plyToUpdatePc = pc.get(currPly);
-			plyToUpdatePc.clear();
-			plyToUpdatePc.add(currMove);
+		if (currPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			length[currPly] = 1;
+			pc[currPly][0] = currMove;
 			int nextPly = currPly+1;
-			if (nextPly < pc.size()) {
+			if (nextPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
 				// Bring down, if possible
-				plyToUpdatePc.addAll(pc.get(nextPly));
+				for (int i=0; i<length[nextPly]; i++) {
+					pc[currPly][i+1] = pc[nextPly][i];
+					length[currPly] += 1;
+				}
 			}
 			if (SearchDebugAgent.DEBUG_ENABLED) sda.printPrincipalContinuation(this);
 		}
@@ -97,32 +87,19 @@ public class PrincipalContinuation {
 	
 	// Update a principal continuation from a Transposition hit where we don't have onwards pv
 	void set(int currPly, int currMove) {
-		if (currPly < pc.size()) {
-			List<Integer> plyToUpdatePc = pc.get(currPly);
-			plyToUpdatePc.clear();
-			plyToUpdatePc.add(currMove);
+		if (currPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			length[currPly] = 1;
+			pc[currPly][0] = currMove;
 			clearContinuationBeyondPly(currPly);
 			if (SearchDebugAgent.DEBUG_ENABLED) sda.printPrincipalContinuation(this);
-		}
-	}
-	
-	// Update a principal continuation from a Transposition hit where we do have an onwards pv
-	public void update(int currPly, List<Integer> onwards_pv) {
-		if (currPly < pc.size()) {
-			List<Integer> plyToUpdatePc = pc.get(currPly);
-			plyToUpdatePc.clear();
-			if (onwards_pv != null) {
-				plyToUpdatePc.addAll(onwards_pv);
-			}
-			clearContinuationBeyondPly(currPly);
 		}
 	}
 	
 	// Clear all downstream pv's, from the current ply
 	void clearContinuationBeyondPly(int currPly) {
 		int nextPly = currPly+1;
-		if (nextPly < pc.size()) {
-			pc.get(nextPly).clear();
+		if (nextPly < EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			length[nextPly] = 0;
 		}
 	}
 }

EubosChess/src/main/java/eubos/search/SearchMetrics.java
@@ -88,7 +88,7 @@ public class SearchMetrics {
 		isScoreBackedUpFromSearch = true;
 	}
 	
-	synchronized void setPrincipalVariationDataFromHash(int extendedSearchDeepestPly, List<Integer> pc, short positionScore) {
+	synchronized void setPrincipalVariationDataFromHash(int extendedSearchDeepestPly, short positionScore) {
 		setCpScore(positionScore);
 		this.cpScore = positionScore;
 	}

EubosChess/src/main/java/eubos/search/generators/MiniMaxMoveGenerator.java
@@ -3,6 +3,9 @@ package eubos.search.generators;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import eubos.board.Piece;
 import eubos.main.EubosEngineMain;
@@ -87,7 +90,8 @@ public class MiniMaxMoveGenerator implements
 			SearchMetricsReporter sr)  {
 		boolean foundMate = false;
 		sm.setDepth(searchDepth);
-		sm.setPrincipalVariation(pc.toPvList(0));
+		List<Integer> list = Arrays.stream(pc.toPvList(0)).boxed().collect(Collectors.toList());
+		sm.setPrincipalVariation(list);
 		ps = new PlySearcher(tta, pc, sm, sr, searchDepth, pm, pos, pe, killers, sda, ml);
 		// Descend the plies in the search tree, to full depth, updating board and scoring positions
 		try {

EubosChess/src/test/java/eubos/search/PrincipalContinuationTest.java
@@ -2,8 +2,9 @@ package eubos.search;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -36,7 +37,7 @@ public class PrincipalContinuationTest {
 		classUnderTest.update(2, Move.valueOf(Position.d2, Piece.WHITE_PAWN, Position.d4, Piece.NONE ));
 		classUnderTest.update(1, Move.valueOf(Position.e7, Piece.BLACK_PAWN, Position.e5, Piece.NONE ));
 		classUnderTest.update(0, Move.valueOf(Position.a2, Piece.WHITE_PAWN, Position.a3, Piece.NONE ));
-		List<Integer> pv = classUnderTest.toPvList(0);
+		List<Integer> pv = Arrays.stream(classUnderTest.toPvList(0)).boxed().collect(Collectors.toList());
 		assertEquals(Move.valueOf(Position.a2, Piece.WHITE_PAWN, Position.a3, Piece.NONE ),(int) pv.get(0));
 		assertEquals(Move.valueOf(Position.e7, Piece.BLACK_PAWN, Position.e5, Piece.NONE ),(int) pv.get(1));
 		assertEquals(Move.valueOf(Position.d2, Piece.WHITE_PAWN, Position.d4, Piece.NONE ), (int)pv.get(2));
@@ -51,7 +52,7 @@ public class PrincipalContinuationTest {
 		classUnderTest.update(1, Move.valueOf(Position.e7, Piece.BLACK_PAWN, Position.e5, Piece.NONE ));
 		classUnderTest.update(0, Move.valueOf(Position.a2, Piece.WHITE_PAWN, Position.a3, Piece.NONE ));
 		classUnderTest.clearContinuationBeyondPly(1);
-		List<Integer> pv = classUnderTest.toPvList(0);
+		List<Integer> pv = Arrays.stream(classUnderTest.toPvList(0)).boxed().collect(Collectors.toList());
 		assertEquals(Move.valueOf(Position.a2, Piece.WHITE_PAWN, Position.a3, Piece.NONE ),(int) pv.get(0));
 		assertEquals(Move.valueOf(Position.e7, Piece.BLACK_PAWN, Position.e5, Piece.NONE ),(int) pv.get(1));
 		assertEquals(2, pv.size());		
@@ -59,23 +60,9 @@ public class PrincipalContinuationTest {
 
 	@Test
 	public void testToPvList_InitialState() {
-		List<Integer> pv = classUnderTest.toPvList(0);
+		List<Integer> pv = Arrays.stream(classUnderTest.toPvList(0)).boxed().collect(Collectors.toList());
 		assertTrue(pv != null);
 		assertTrue(pv.isEmpty());
-	}
-
-	@Test
-	public void testUpdateFromHashHit(){
-		List<Integer> source_pc = new ArrayList<Integer>();
-		source_pc.add(Move.valueOf(Position.e2, Piece.NONE, Position.e4, Piece.NONE ));
-		source_pc.add(Move.valueOf(Position.e7, Piece.NONE, Position.e5, Piece.NONE ));
-		source_pc.add(Move.valueOf(Position.d2, Piece.NONE, Position.d4, Piece.NONE ));
-		source_pc.add(Move.valueOf(Position.e5, Piece.NONE, Position.d4, Piece.NONE ));
-		classUnderTest.update(3, source_pc);
-		classUnderTest.update(2, Move.valueOf(Position.a7, Piece.NONE, Position.a6, Piece.NONE ));
-		List<Integer> updated_pc = classUnderTest.toPvList(2);
-		assertEquals(source_pc, updated_pc.subList(1, updated_pc.size()));
-		assertEquals((int)Move.valueOf(Position.a7, Piece.NONE, Position.a6, Piece.NONE ), (int)updated_pc.get(0));
 	}
 
 	@Test
@@ -87,7 +74,7 @@ public class PrincipalContinuationTest {
 	@Test
 	public void testUpdateAtPly0WhenEnpty() {
 		classUnderTest.update(0, Move.valueOf(Position.e2, Piece.NONE, Position.e4, Piece.NONE ));
-		List<Integer> updated_pc = classUnderTest.toPvList(0);
+		List<Integer> updated_pc = Arrays.stream(classUnderTest.toPvList(0)).boxed().collect(Collectors.toList());
 		assertFalse(updated_pc.isEmpty());
 		assertEquals((int)Move.valueOf(Position.e2, Piece.NONE, Position.e4, Piece.NONE ), (int)updated_pc.get(0));
 	}

commit 7de2f4741bd1718c601e0ce6e87d9d3fa32854b2

Make sure the best move at each ply is used- correct defect introduced in 45a0e23.

EubosChess/src/main/java/eubos/search/PlySearcher.java
@@ -84,7 +84,6 @@ public class PlySearcher {
 	
 	private volatile boolean terminate = false;
 	
-	private int[] lastPc;
 	private ITranspositionAccessor tt;
 	private SearchMetricsReporter sr;
 	private KillerList killers;
@@ -124,7 +123,6 @@ public class PlySearcher {
 		this.pos = pos;
 		this.pe = pe;
 		this.sr = sr;
-		this.lastPc = pc.toPvList(0);
 		this.sda = sda;
 		originalSearchDepthRequiredInPly = searchDepthPly;
 		
@@ -219,8 +217,7 @@ public class PlySearcher {
 		
 		// This move is only valid for the principal continuation, for the rest of the search, it is invalid. It can also be misleading in iterative deepening?
 		// It will deviate from the hash move when we start updating the hash during iterative deepening.
-		prevBestMove[0] = ((lastPc != null) && (lastPc.length > 0)) ? lastPc[0] : Move.NULL_MOVE;
-		prevBestMove[0] =  Move.clearBest(prevBestMove[0]);
+		prevBestMove[0] = Move.clearBest(pc.getBestMove((byte)0));
 		if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) pc.clearContinuationBeyondPly(0);
 		
 		if (SearchDebugAgent.DEBUG_ENABLED) {
@@ -348,8 +345,7 @@ public class PlySearcher {
 						
 		// This move is only valid for the principal continuation, for the rest of the search, it is invalid. It can also be misleading in iterative deepening?
 		// It will deviate from the hash move when we start updating the hash during iterative deepening.
-		prevBestMove[currPly] = ((lastPc != null) && (lastPc.length > currPly)) ? lastPc[currPly] : Move.NULL_MOVE;
-		prevBestMove[currPly] =  Move.clearBest(prevBestMove[currPly]);
+		prevBestMove[currPly] = Move.clearBest(pc.getBestMove(currPly));
 		if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) pc.clearContinuationBeyondPly(currPly);
 		
 		// Check for draws by three-fold repetition

EubosChess/src/main/java/eubos/search/PrincipalContinuation.java
@@ -14,7 +14,7 @@ public class PrincipalContinuation {
 		// Create the pc list at each ply
 		pc = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY][];
 		for (int i = 0; i < EubosEngineMain.SEARCH_DEPTH_IN_PLY; i++) {
-			pc[i] = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY];
+			pc[i] = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY-i];
 		}
 		length = new int[EubosEngineMain.SEARCH_DEPTH_IN_PLY];
 		this.sda = sda;