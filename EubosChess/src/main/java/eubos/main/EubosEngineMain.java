666783c9d1bacca332f9aac5ab4f5fbcb2bc35a1
Chris Bolt <c_j_bolt@yahoo.co.uk>
2022-06-12 15:50:05 +0100

    Roll to version 2.13 development

--- EubosChess/src/main/java/eubos/main/EubosEngineMain.java
+++ EubosChess/src/main/java/eubos/main/EubosEngineMain.java
@@ -50,7 +50,7 @@ import java.util.logging.*;
 public class EubosEngineMain extends AbstractEngine {
 	
 	static final int EUBOS_MAJOR_VERSION = 2;
-	static final int EUBOS_MINOR_VERSION = 12;
+	static final int EUBOS_MINOR_VERSION = 13;
 	
 	public static final byte SEARCH_DEPTH_IN_PLY = Byte.MAX_VALUE;
 	public static final int DEFAULT_NUM_SEARCH_THREADS = 1;
