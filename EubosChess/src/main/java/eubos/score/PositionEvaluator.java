ecae28c89edbe25c6e6c457cd6e2340631572a58
Chris Bolt <c_j_bolt@yahoo.co.uk>
2022-10-03 18:31:05 +0100

    Test disabled candidate eval, passed pawn update only after opening etc

--- EubosChess/src/main/java/eubos/score/PositionEvaluator.java
+++ EubosChess/src/main/java/eubos/score/PositionEvaluator.java
@@ -36,7 +36,7 @@ public class PositionEvaluator implements IEvaluate {
 	public static final boolean ENABLE_PAWN_EVALUATION = true;
 	public static final boolean ENABLE_PAWN_HASH_TABLE = true;
 	public static final boolean ENABLE_KPK_EVALUATION = true;
-	public static final boolean ENABLE_CANDIDATE_PP_EVALUATION = true;
+	public static final boolean ENABLE_CANDIDATE_PP_EVALUATION = false;
 	public static final boolean ENABLE_PP_IMBALANCE_EVALUATION = false;
 	
 	public static final boolean ENABLE_KING_SAFETY_EVALUATION = true;
