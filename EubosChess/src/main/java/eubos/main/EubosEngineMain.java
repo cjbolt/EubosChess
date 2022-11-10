6dd6f71c662c4f3b72cae20219a657097f8fe70b
Chris Bolt <c_j_bolt@yahoo.co.uk>
2022-11-10 17:53:23 +0000

    Re-enable NMR

--- EubosChess/src/main/java/eubos/main/EubosEngineMain.java
+++ EubosChess/src/main/java/eubos/main/EubosEngineMain.java
@@ -71,7 +71,7 @@ public class EubosEngineMain extends AbstractEngine {
 	public static final boolean ENABLE_ASPIRATION_WINDOWS = true;
 	public static final boolean ENABLE_LAZY_EVALUATION = true;	
 	public static final boolean ENABLE_LATE_MOVE_REDUCTION = true;
-	public static final boolean ENABLE_NULL_MOVE_PRUNING = false;
+	public static final boolean ENABLE_NULL_MOVE_PRUNING = true;
 	public static final boolean ENABLE_STAGED_MOVE_GENERATION = true;
 	public static final boolean ENABLE_COUNTED_PASSED_PAWN_MASKS = true;
 	
