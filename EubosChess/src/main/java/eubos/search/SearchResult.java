e1df8c37eda26ae66db46de0ba9197655688a027
Chris Bolt <c_j_bolt@yahoo.co.uk>
2022-12-05 08:13:53 +0000

    Addd debug report to Search Result

--- EubosChess/src/main/java/eubos/search/SearchResult.java
+++ EubosChess/src/main/java/eubos/search/SearchResult.java
@@ -1,6 +1,7 @@
 package eubos.search;
 
 import eubos.position.Move;
+import eubos.search.transposition.Transposition;
 
 public class SearchResult {
 	public int[] pv;
@@ -21,5 +22,20 @@ public class SearchResult {
 		this.foundMate = foundMate;
 		this.rootTrans = rootTransposition;
 		this.depth = depth;
+	}
+
+	public String report() {
+		StringBuilder sb = new StringBuilder();
+		for (int move: pv) {
+			if (move == Move.NULL_MOVE) break;
+			sb.append(Move.toString(move));
+			sb.append(' ');
+		}
+		String output = String.format("result: pv=%s, mate=%s, depth=%d, rootTrans=%s", 
+				sb.toString(),
+				foundMate,
+				depth,
+				(rootTrans != 0L) ? Transposition.report(rootTrans) : "0L");
+		return output;
 	}
 }
