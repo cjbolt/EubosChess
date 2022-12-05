8129c1f8184aec165ae738b176e64c09ef791783
Chris Bolt <c_j_bolt@yahoo.co.uk>
2022-12-05 08:15:03 +0000

    Handle possible null pv when only illegal move

--- EubosChess/src/test/java/eubos/search/generators/MiniMaxMoveGeneratorTest.java
+++ EubosChess/src/test/java/eubos/search/generators/MiniMaxMoveGeneratorTest.java
@@ -141,7 +141,7 @@ public class MiniMaxMoveGeneratorTest {
 		// 1 kP......
 		//   abcdefgh
 		setupPosition( "8/8/8/8/8/1pp5/ppp5/Kp6 w - - - 1" );
-		assertEquals(Move.NULL_MOVE, classUnderTest.findMove(SEARCH_DEPTH_IN_PLY).pv[0]);
+		assertNull(classUnderTest.findMove(SEARCH_DEPTH_IN_PLY).pv);
 	}
 	
 	@Test
