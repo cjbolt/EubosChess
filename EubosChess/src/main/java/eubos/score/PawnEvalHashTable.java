1631a138a6d2b16da91232e916725ca9c5df1264
Chris Bolt <c_j_bolt@yahoo.co.uk>
2023-03-14 10:35:09 +0000

    Object based pawn hash table

--- EubosChess/src/main/java/eubos/score/PawnEvalHashTable.java
+++ EubosChess/src/main/java/eubos/score/PawnEvalHashTable.java
@@ -1,10 +1,15 @@
 package eubos.score;
 
-import java.util.Arrays;
-
 public class PawnEvalHashTable {	
-	private long [] lower = null;
-	private long [] upper = null;
+	
+	class PawnHashEntry {
+		long black;
+		long white;
+		short phase;
+		short score;
+	};
+	
+	PawnHashEntry[] table;
 	
 	public static final int WHITE_SCALING_FOR_UPPER = (4*8)-8; // most significant 4 bytes of white pawns in upper
 	public static final int WHITE_SCALING_FOR_LOWER = (6*8)-8; // least significant 2 bytes of white pawns in lower
@@ -15,36 +20,34 @@ public class PawnEvalHashTable {
 	public static final int PAWN_HASH_MASK = (int)(Long.highestOneBit(SIZE_OF_PAWN_HASH)-1);
 		
 	public PawnEvalHashTable() {
-		lower = new long[SIZE_OF_PAWN_HASH];
-		upper = new long[SIZE_OF_PAWN_HASH];
-		Arrays.fill(upper, Short.MAX_VALUE << 48);
+		table = new PawnHashEntry[SIZE_OF_PAWN_HASH];
+		for (int i=0; i<SIZE_OF_PAWN_HASH; i++) {
+			table[i] = new PawnHashEntry();
+		}
 	}
 	
 	public synchronized short get(int hash, int phase_weighting, long white_pawns, long black_pawns, boolean onMoveIsWhite) {
-		int index = hash & PAWN_HASH_MASK;
-		if (lower[index] == ((black_pawns >>> BLACK_SCALING_FOR_LOWER) | (white_pawns << WHITE_SCALING_FOR_LOWER))) {
-			long composite = upper[index];
-			if ((composite & 0xFFFF_FFFFL) == ((int)(white_pawns >>> WHITE_SCALING_FOR_UPPER))) {
-				if (((composite >>> 32) & 0xFFL) == phase_weighting) {
-					short score = (short) (composite >> 48);
-					// Score saved in table is from white point of view
-					if (!onMoveIsWhite) {
-						score = (short)-score;
-					}
-					return score;
-				}
+		PawnHashEntry entry = table[hash & PAWN_HASH_MASK];
+		if (entry.black == black_pawns && entry.white == white_pawns && entry.phase == phase_weighting) {
+			short score = entry.score;
+			if (!onMoveIsWhite) {
+				// Score saved in table is from white point of view
+				score = (short)-score;
 			}
+			return score;
 		}
 		return Short.MAX_VALUE;
 	}
 	
 	public synchronized void put(int hash, int phase_weighting, int score, long white_pawns, long black_pawns, boolean onMoveIsWhite) {
-		int index = hash & PAWN_HASH_MASK;
-		// Score saved in table is from white point of view
+		PawnHashEntry entry = table[hash & PAWN_HASH_MASK];
 		if (!onMoveIsWhite) {
+			// Score saved in table is from white point of view
 			score = -score;
 		}
-		lower[index] = (black_pawns >>> BLACK_SCALING_FOR_LOWER) | (white_pawns << WHITE_SCALING_FOR_LOWER);
-		upper[index] = (white_pawns >>> WHITE_SCALING_FOR_UPPER) | (((long)phase_weighting) << 32) | (((long)score) << 48);
+		entry.black = black_pawns;
+		entry.white = white_pawns;
+		entry.phase = (short)phase_weighting;
+		entry.score = (short)score;
 	}
 }
