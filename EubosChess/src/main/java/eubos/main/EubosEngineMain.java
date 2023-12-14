217082262b94335b59fb9d049c6c3ae75c8fd8e5
Chris Bolt <c_j_bolt@yahoo.co.uk>
2023-12-14 18:14:45 +0000

    Restore lazy evaluation, it is a net benefit.

--- EubosChess/src/main/java/eubos/main/EubosEngineMain.java
+++ EubosChess/src/main/java/eubos/main/EubosEngineMain.java
@@ -84,7 +84,7 @@ public class EubosEngineMain extends AbstractEngine {
 	public static final boolean ENABLE_QUIESCENCE_CHECK = true;
 	public static final boolean ENABLE_ASPIRATION_WINDOWS = true;
 	public static final boolean ENABLE_SKEWED_ASPIRATION_WINDOWS = false;
-	public static final boolean ENABLE_LAZY_EVALUATION = false;	
+	public static final boolean ENABLE_LAZY_EVALUATION = true;	
 	public static final boolean ENABLE_LATE_MOVE_REDUCTION = true;
 	public static final boolean ENABLE_NULL_MOVE_PRUNING = true;
 	public static final boolean ENABLE_STAGED_MOVE_GENERATION = true;
