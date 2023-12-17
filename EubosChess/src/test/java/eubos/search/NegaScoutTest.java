f8db61923e1d79ccb1513ee8f8c0f7434938efee
Chris Bolt <c_j_bolt@yahoo.co.uk>
2023-12-17 11:23:09 +0000

    Simple test harness for negaScout

---
+++ EubosChess/src/test/java/eubos/search/NegaScoutTest.java
@@ -0,0 +1,153 @@
+package eubos.search;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.fluxchess.jcpi.models.IllegalNotationException;
+
+import eubos.board.BitBoard;
+import eubos.board.Piece;
+import eubos.main.EubosEngineMain;
+import eubos.position.IChangePosition;
+import eubos.position.IPositionAccessors;
+import eubos.position.Move;
+import eubos.position.MoveList;
+import eubos.position.MoveListIterator;
+import eubos.position.PositionManager;
+import eubos.score.IEvaluate;
+import eubos.search.PlySearcher.SearchState;
+import eubos.search.transposition.FixedSizeTranspositionTable;
+import eubos.search.transposition.ITranspositionAccessor;
+
+public class NegaScoutTest {
+
+	int currPly = 0;
+	SearchState state[];
+	private IChangePosition pm;
+	private IPositionAccessors pos;
+	private IEvaluate pe;
+	private PrincipalContinuation pc;
+	private SearchMetrics sm;
+	private SearchDebugAgent sda;
+	private ITranspositionAccessor tt;
+	private KillerList killers;
+	public long rootTransposition = 0L;
+	private MoveList ml;
+	
+	protected void setupPosition(String fen) {
+		PositionManager posMgr = new PositionManager( fen );
+		pm = posMgr;
+		pos = posMgr;
+		sm = new SearchMetrics(pos);
+		killers = new KillerList();
+		sda = new SearchDebugAgent(pos.getMoveNumber(), pos.getOnMove() == Piece.Colour.white);
+		pc = new PrincipalContinuation(EubosEngineMain.SEARCH_DEPTH_IN_PLY, sda);
+		ml = new MoveList((PositionManager)pm, 1);
+		tt = new FixedSizeTranspositionTable();
+		pe = posMgr.getPositionEvaluator();
+		PlySearcher ps = new PlySearcher(tt, pc, sm, null, (byte) 5, pm, pos, pe, killers, sda, ml);
+		currPly = 0;
+		state = new SearchState[EubosEngineMain.SEARCH_DEPTH_IN_PLY+1];
+		for (int i=0; i < state.length; i++) {
+			state[i] = ps.new SearchState();
+		}
+	}
+	
+	@Test
+	public void test_mateInTwo() throws IllegalNotationException {
+		setupPosition( "k1K5/b7/R7/1P6/1n6/8/8/8 w - - - 1");
+		int score = negaScout(5, -Score.PROVISIONAL_BETA, -Score.PROVISIONAL_ALPHA);
+		
+		assertTrue(Score.isMate((short)score));
+		assertEquals(Score.PROVISIONAL_BETA-3, score); // Prov, Matein1, matein2
+		
+		int expectedMove = Move.valueOfBit(BitBoard.b5, Piece.WHITE_PAWN, BitBoard.b6, Piece.NONE);
+		int bestMove = pc.getBestMove((byte)0);
+		assertTrue(Move.areEqual(expectedMove, bestMove));
+	}
+	
+	private int negaScout(int depth, int alpha, int beta) {
+		
+		SearchState s = state[currPly];
+		s.update();
+		s.initialise(currPly, alpha, beta);
+		
+		// Check for absolute draws
+		if (pos.isThreefoldRepetitionPossible() || pos.isInsufficientMaterial()) return 0;
+		
+		// Absolute depth limit
+		if (currPly >= EubosEngineMain.SEARCH_DEPTH_IN_PLY) {
+			return pe.getFullEvaluation();
+		}
+		if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) pc.clearContinuationBeyondPly(currPly);
+		
+		// Extend search for in-check scenarios, treated outside of quiescence search
+		if (s.inCheck) {
+			++depth;
+		}
+		
+		if (depth <= 0) {
+			//return extendedSearch(s.alpha, s.beta, depth-1);
+			return pe.getFullEvaluation();
+		}
+				
+		// Main search loop for this ply
+		int bestMove = Move.NULL_MOVE;
+		int currMove = Move.NULL_MOVE;
+		int positionScore = s.bestScore;
+		boolean refuted = false;
+		MoveListIterator move_iter = ml.initialiseAtPly(s.prevBestMove, killers.getMoves(currPly), s.inCheck, false, currPly, depth == 1);
+		while ((currMove = move_iter.nextInt()) != Move.NULL_MOVE && !refuted) {
+			
+			if (!pm.performMove(currMove)) { continue; }
+			
+			s.moveNumber += 1;
+			if (s.moveNumber == 1) {
+				pc.initialise(currPly, currMove);
+				bestMove = currMove;
+			}
+			
+			if (SearchDebugAgent.DEBUG_ENABLED) sda.printNormalSearch(s.alpha, s.beta);
+			if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) pc.clearContinuationBeyondPly(currPly);
+			if (SearchDebugAgent.DEBUG_ENABLED) sda.printPerformMove(currMove);
+			if (SearchDebugAgent.DEBUG_ENABLED) sda.nextPly();
+			currPly++;
+			
+			//s.update();
+			positionScore = -negaScout(depth-1, -s.beta, -s.alpha);
+			
+			pm.unperformMove();
+			currPly--;
+			if (SearchDebugAgent.DEBUG_ENABLED) sda.prevPly();
+			if (SearchDebugAgent.DEBUG_ENABLED) sda.printUndoMove(currMove, positionScore);
+			if (EubosEngineMain.ENABLE_UCI_INFO_SENDING) sm.incrementNodesSearched();
+			
+			// Handle score backed up to this node
+			if (positionScore > s.alpha) {
+				s.alpha = s.bestScore = positionScore;
+				bestMove = currMove;
+				if (s.alpha >= s.beta) {
+					killers.addMove(currPly, bestMove);
+					if (SearchDebugAgent.DEBUG_ENABLED) sda.printRefutationFound(s.bestScore);
+					refuted = true;
+					break;
+				}
+				pc.update(currPly, bestMove);
+			} 
+			else if (positionScore > s.bestScore) {
+				bestMove = currMove;
+				s.bestScore = positionScore;
+			}
+		}
+
+		if (s.moveNumber == 0) {
+			// No moves searched at this point means either a stalemate or checkmate has occurred
+			return s.inCheck ? Score.getMateScore(currPly) : 0;
+		}
+		
+		return s.bestScore;
+	}
+	
+}
