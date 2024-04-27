93b9d2989c0b593a65635cec1303ed7a67efe678
Chris Bolt <c_j_bolt@yahoo.co.uk>
2024-04-27 09:17:31 +0100

    Turn off IID, it doesn't improve performance.

--- EubosChess/src/main/java/eubos/main/EubosEngineMain.java
+++ EubosChess/src/main/java/eubos/main/EubosEngineMain.java
@@ -83,7 +83,7 @@ public class EubosEngineMain extends AbstractEngine {
 	public static final boolean ENABLE_NULL_MOVE_PRUNING = true;
 	public static final boolean ENABLE_STAGED_MOVE_GENERATION = true;
 	public static final boolean ENABLE_COUNTED_PASSED_PAWN_MASKS = true;
-	public static final boolean ENABLE_ITERATIVE_DEEPENING = true;
+	public static final boolean ENABLE_ITERATIVE_DEEPENING = false;
 	public static final boolean ENABLE_FUTILITY_PRUNING = true;
 	public static final boolean ENABLE_PINNED_TO_KING_CHECK_IN_ILLEGAL_DETECTION = true;
 	public static final boolean ENABLE_PER_MOVE_FUTILITY_PRUNING = true;
