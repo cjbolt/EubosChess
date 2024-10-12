500ebc350e76500f8da8787e3029ae25026a1f66
Chris Bolt <c_j_bolt@yahoo.co.uk>
2024-10-12 17:36:13 +0100

    Just pawn bitboard check, TC1+1 8927games 39.9%

--- EubosChess/src/main/java/eubos/score/PawnEvalHashTable.java
+++ EubosChess/src/main/java/eubos/score/PawnEvalHashTable.java
@@ -4,38 +4,38 @@ import java.util.Arrays;
 
 public class PawnEvalHashTable {
 	
-	private long [] lower = null;
+	//private long [] lower = null;
 	private long [] upper = null;
 	
-	private static final int WHITE_SCALING_FOR_UPPER = (4*8)-8; // most significant 4 bytes of white pawns in upper
-	private static final int WHITE_SCALING_FOR_LOWER = (6*8)-8; // least significant 2 bytes of white pawns in lower
+	//private static final int WHITE_SCALING_FOR_UPPER = (4*8)-8; // most significant 4 bytes of white pawns in upper
+	//private static final int WHITE_SCALING_FOR_LOWER = (6*8)-8; // least significant 2 bytes of white pawns in lower
 	private static final int BLACK_SCALING_FOR_LOWER = 8;       // all 6 bytes of black pawns in lower (shift out 1st rank)
 		
-	private static final int SIZE_OF_PAWN_HASH = 65536;
+	private static final int SIZE_OF_PAWN_HASH = 65536*2;
 	private static final int PAWN_HASH_MASK = (int)(Long.highestOneBit(SIZE_OF_PAWN_HASH)-1);
 	
 	public PawnEvalHashTable() {
-		lower = new long[SIZE_OF_PAWN_HASH];
+		//lower = new long[SIZE_OF_PAWN_HASH];
 		upper = new long[SIZE_OF_PAWN_HASH];
 		Arrays.fill(upper, Short.MAX_VALUE << 48);
 	}
 	
-	private long createLower(long white_pawns, long black_pawns) {
-		return (black_pawns >>> BLACK_SCALING_FOR_LOWER) | (white_pawns << WHITE_SCALING_FOR_LOWER);
-	}
+//	private long createLower(long white_pawns, long black_pawns) {
+//		return (black_pawns >>> BLACK_SCALING_FOR_LOWER) | (white_pawns << WHITE_SCALING_FOR_LOWER);
+//	}
 	
 	public synchronized short get(int hash, long white_pawns, long black_pawns, boolean onMoveIsWhite) {
 		int index = hash & PAWN_HASH_MASK;
-		if (lower[index] == createLower(white_pawns, black_pawns)) {
-			long composite = upper[index];
-			if ((composite & 0xFFFF_FFFFL) == ((int)(white_pawns >>> WHITE_SCALING_FOR_UPPER))) {
-				short score = (short) (composite >> 48);
+		if ((upper[index] & 0xFFFF_FFFF_FFFFL) == (white_pawns|black_pawns) >>> BLACK_SCALING_FOR_LOWER) {
+			//long composite = upper[index];
+			//if ((composite & 0xFFFF_FFFFL) == ((int)(white_pawns >>> WHITE_SCALING_FOR_UPPER))) {
+				short score = (short) (upper[index] >> 48);
 				// Score saved in table is from white point of view
 				if (!onMoveIsWhite) {
 					score = (short)-score;
 				}
 				return score;
-			}
+			//}
 		}
 		return Short.MAX_VALUE;
 	}
@@ -46,7 +46,7 @@ public class PawnEvalHashTable {
 		if (!onMoveIsWhite) {
 			score = -score;
 		}
-		lower[index] = createLower(white_pawns, black_pawns);
-		upper[index] = (white_pawns >>> WHITE_SCALING_FOR_UPPER) | (((long)score) << 48);
+		//lower[index] = createLower(white_pawns, black_pawns);
+		upper[index] = ((white_pawns|black_pawns) >>> BLACK_SCALING_FOR_LOWER) | (((long)score) << 48);
 	}
 }
