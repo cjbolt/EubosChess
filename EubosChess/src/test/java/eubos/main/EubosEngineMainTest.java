7f041236c26069479a194c81d718b9abe40e4687
cjbolt <c_j_b@DESKTOP-H282E9K>
2025-07-29 21:45:50 +0100

    Revert "Fix up unit tests for release NN."
    This reverts commit 8e9b63d40d8c14669f83fd19a4b0f3c9b4c7f3b0.

--- EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
+++ EubosChess/src/test/java/eubos/main/EubosEngineMainTest.java
@@ -182,7 +182,7 @@ public class EubosEngineMainTest extends AbstractEubosIntegration {
 		setupEngine();
 		commands.add(new CommandPair(POS_FEN_PREFIX+"5Q2/6K1/8/3k4/8/8/8/8 w - - 1 113"+CMD_TERMINATOR, null));
 		commands.add(new CommandPair(GO_DEPTH_PREFIX+"11"+CMD_TERMINATOR, BEST_PREFIX+"f8b4"+CMD_TERMINATOR));
-		assertTrue(performTestExpectMate(15000, 10));
+		assertTrue(performTestExpectMate(15000, 7));
 	}
 	
 	@Test
@@ -220,8 +220,8 @@ public class EubosEngineMainTest extends AbstractEubosIntegration {
 		int mateDepth = 0;
 		setupEngine();
 		commands.add(new CommandPair(POS_FEN_PREFIX+"8/8/8/3K1k2/8/8/8/7r b - - 5 111"+CMD_TERMINATOR, null));
-		commands.add(new CommandPair(GO_TIME_PREFIX+"14000"+CMD_TERMINATOR, BEST_PREFIX+"h1d1"+CMD_TERMINATOR));
-		mateDepth = 16;
+		commands.add(new CommandPair(GO_TIME_PREFIX+"14000"+CMD_TERMINATOR, BEST_PREFIX+"h1h4"+CMD_TERMINATOR));
+		mateDepth = 17;
 		assertTrue(performTestExpectMate(14000, mateDepth));
 	}
 	 
