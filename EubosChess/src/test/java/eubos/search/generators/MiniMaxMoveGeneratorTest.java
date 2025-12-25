a20110c2656441becf70dfbde80fd0ea4fa55250
Chris Bolt <c_j_bolt@yahoo.co.uk>
2025-12-25 23:32:30 +0000

    Add test for insufficient

--- EubosChess/src/test/java/eubos/search/generators/MiniMaxMoveGeneratorTest.java
+++ EubosChess/src/test/java/eubos/search/generators/MiniMaxMoveGeneratorTest.java
@@ -417,55 +417,13 @@ public class MiniMaxMoveGeneratorTest {
 		// Leads to stalemate
 		assertEquals(expectedMove, Move.toGenericMove(res.pv[0]));
 	}
-	
+			
 	@Test
-	@Ignore // confusing as the position is unreal - e.g. stalemate! Perhaps add Kings???
-	public void test_extendedSearch_recaptureBishopLeadsToLossOfMaterial() throws IllegalNotationException {
-		setupPosition("8/6q1/5p2/8/8/2B5/8/8 w - - 0 38 ");
-		expectedMove = new GenericMove("c3f6");
-		
-		SearchResult res = classUnderTest.findMove((byte)1);
-		
-		assertNotEquals(expectedMove, res.pv[0]);
-	}
-	
-	@Test
-	@Ignore // Not a good test - nowadays it isn't clear eubos should castle.
-	public void test_findMove_NeedToCastle_FromLichess1() throws IllegalNotationException {
-		setupPosition( "rnb1kbnr/p4p1p/1qp1p1p1/3p4/8/1B2PN2/PPPP1PPP/RNBQK2R w KQkq - - -");
-		expectedMove = new GenericMove("e1g1");
-		
-		SearchResult res = classUnderTest.findMove((byte)3);
-		
-		assertEquals(expectedMove, res.pv[0]);
-	}
-	
-	@Test
-	@Ignore // Don't usually run, takes ~12 seconds from an empty hashTable!
 	public void test_assymetric_eval_due_to_zugzwang() throws IllegalNotationException {
-		setupPosition("8/8/8/3K2k1/5b2/5p2/5B2/8 w - - - 61");
-		SearchResult res = classUnderTest.findMove((byte)11);
-		
-		// Check the move and score are as expected
-		expectedMove = new GenericMove("d5e4");
+		setupPosition("4R3/8/8/6P1/4p2B/2bb4/4kPP1/6K1 w - - 5 105 ");
+		SearchResult res = classUnderTest.findMove((byte)12);
+		expectedMove = new GenericMove("g5g6");
 		assertEquals(expectedMove, Move.toGenericMove(res.pv[0]));
-		assertEquals(-139, classUnderTest.getScore());
-		
-		// Check the hash score is updated properly, negated for white POV
-		pm.performMove(res.pv[0]);
-		assertEquals(139,Transposition.getScore(hashMap.getTransposition(pm.getHash())));
-	}
-	
-	@Test
-	@Ignore // Don't usually run, takes ~7 seconds from an empty hashTable!
-	public void test_assymetric_eval_due_to_zugzwang_from_black() throws IllegalNotationException {
-		setupPosition("8/8/8/6k1/4Kb2/5p2/5B2/8 b - - 1 1");
-		SearchResult res = classUnderTest.findMove((byte)11);
-		
-		// Check the move and score are as expected
-		expectedMove = new GenericMove("g5g4");
-		assertEquals(expectedMove, Move.toGenericMove(res.pv[0]));
-		assertEquals(157, classUnderTest.getScore());
 	}
 	
 	@Test
