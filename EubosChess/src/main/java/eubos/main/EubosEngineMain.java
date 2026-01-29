c738d856d21237fa078f0a0240e7669fb75a02c3
Chris Bolt <c_j_bolt@yahoo.co.uk>
2026-01-29 22:49:52 +0000

    Set version

--- EubosChess/src/main/java/eubos/main/EubosEngineMain.java
+++ EubosChess/src/main/java/eubos/main/EubosEngineMain.java
@@ -55,7 +55,7 @@ import java.util.Set;
 public class EubosEngineMain extends AbstractEngine {
 	
 	static final int EUBOS_MAJOR_VERSION = 4;
-	static final int EUBOS_MINOR_VERSION = 3;
+	static final int EUBOS_MINOR_VERSION = 4;
 	
 	public static final byte SEARCH_DEPTH_IN_PLY = Byte.MAX_VALUE;
 	public static final int DEFAULT_NUM_SEARCH_THREADS = 1;
